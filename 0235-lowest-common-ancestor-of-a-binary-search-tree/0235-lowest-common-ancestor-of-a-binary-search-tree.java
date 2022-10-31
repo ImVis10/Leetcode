11fcd5dea383cd6185a1afc4fb1c483c3c9571e8
Elio <41447494+ImVis10@users.noreply.github.com>
2022-10-31 18:20:50 -0400

    Time: 20 ms (19.04%), Space: 49.2 MB (80.24%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -10,7 +10,7 @@
 
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        // lowest common ancestor - first common ancestor when traversing bottom up i.e. from the leaf nodes (if the leaf nodes are the given inputs)
+        // lowest common ancestor - first common ancestor when traversing bottom up i.e. from the given input nodes
         // a node can be a descendant of itself.
         // both the given nodes can be in the left sub tree or can be in the right sub tree.
         // If one is in the left and one is in the right, it is obvious that root is the lowest and only common ancestor.
