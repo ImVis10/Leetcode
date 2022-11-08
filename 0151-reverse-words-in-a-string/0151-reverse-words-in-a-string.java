da46bc792f0ff2570e71051da0554449dd4f1a1f
Elio <41447494+ImVis10@users.noreply.github.com>
2022-11-07 20:51:49 -0500

    Time: 4 ms (97.14%), Space: 43.1 MB (72.28%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,10 +1,42 @@
 class Solution {
+    // public String reverseWords(String s) {
+    //     String[] words = s.split("\\s+"); // splits the string for one or multiple spaces.
+    //     StringBuilder sb = new StringBuilder();
+    //     for (int i = words.length - 1; i >= 0; i--) {
+    //         sb.append(words[i]).append(" ");
+    //     }
+    //     return sb.toString().trim();
+    // }
+
+    /** Took from comment section, no extra space used.**/
     public String reverseWords(String s) {
-        String[] words = s.split("\\s+"); // splits the string for one or multiple spaces.
-        StringBuilder sb = new StringBuilder();
-        for (int i = words.length - 1; i >= 0; i--) {
-            sb.append(words[i]).append(" ");
+        if (s.length() < 1) return s; // empty string
+        int startIdx = 0;
+        char[] str = s.toCharArray();
+        // reverse whole string
+        reverse(str, 0, str.length - 1);
+        // reverse word one by one
+        for (int i = 0; i < str.length; i++) {
+            if (str[i] != ' ') {
+                if (startIdx != 0) {
+                 str[startIdx++] = ' ';  // put a space at the beginning of a word. 
+                } 
+                int j = i;
+                while (j < str.length && str[j] != ' ') { // get the entire word until a space arrives.
+                    str[startIdx++] = str[j++];
+                }   
+                reverse(str, startIdx - (j - i), startIdx - 1); // reverse the word
+                i = j;
+            }
         }
-        return sb.toString().trim();
+        return new String(str, 0, startIdx);
+    }
+
+    private void reverse(char[] str, int start, int end) {
+        for (; start < end; start++, end--) {
+            char temp = str[start];
+            str[start] = str[end];
+            str[end] = temp;
+        }
     }
 }
