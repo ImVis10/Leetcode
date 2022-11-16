0680db1fd854454f5532bb06fc438064ca5791ce
Elio <41447494+ImVis10@users.noreply.github.com>
2022-11-16 15:48:49 -0500

    Time: 14 ms (20.20%), Space: 54.2 MB (57.45%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.java
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.java
@@ -3,7 +3,7 @@ class Solution {
         int[] res = new int[grid[0].length]; // one for each ball
         for (int col = 0; col < grid[0].length; col++) { // for every ball
             int currCol = col;
-            for (int row  = 0; row < grid.length; row++) {
+            for (int row  = 0; row < grid.length; row++) { // ball goes to next column only if the cell and its adjacent cell(in next column) have the same value. But the next column can be right or left sloped. So to calculate next column we can either do currCol - 1 or currCol + 1 i.e. currCol + grid[row][nextCol]. Ball can't go if the ball hits either of the walls as well i.e. 0, numColumns  - 1
                 int nextCol = currCol + grid[row][currCol];
                 if (nextCol < 0 || nextCol > grid[0].length - 1 || grid[row][currCol] != grid[row][nextCol]) {
                     res[col] = -1;
