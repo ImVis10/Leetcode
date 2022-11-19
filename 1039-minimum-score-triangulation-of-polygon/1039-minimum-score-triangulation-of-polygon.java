98709c3a5a7b6bfcf99d0fc62760bcb1da17c17f
Elio <41447494+ImVis10@users.noreply.github.com>
2022-11-19 09:03:40 -0500

    Time: 4 ms (85.64%), Space: 42 MB (23.59%) - LeetHub

--- 1039-minimum-score-triangulation-of-polygon/1039-minimum-score-triangulation-of-polygon.java
+++ 1039-minimum-score-triangulation-of-polygon/1039-minimum-score-triangulation-of-polygon.java
@@ -5,9 +5,7 @@ class Solution {
         // a polygon is triangulated into n - 2 triangles
         int n = values.length;
         int[][] dp = new int[n][n];
-        for (int[] row : dp) {
-            Arrays.fill(row, -1);
-        }
+
         return pickThirdVertexAndSolveForSubProblems(values, 0, n - 1, dp);
         
     }
@@ -15,12 +13,18 @@ class Solution {
     // shit.. forgot to cache the results. Got time limit exceeded.
     private int pickThirdVertexAndSolveForSubProblems(int[] values, int i, int j, int[][] dp) {
         if (i + 1 == j) return 0; // base case
-        if (dp[i][j] != -1) return dp[i][j]; // return from cache if its already computed
+        if (dp[i][j] != 0) return dp[i][j]; // return from cache if its already computed
         int res = Integer.MAX_VALUE;
         for(int k = i + 1; k < j; k++) {
             res = Math.min((values[i]*values[k]*values[j] + pickThirdVertexAndSolveForSubProblems(values, i , k, dp) + pickThirdVertexAndSolveForSubProblems(values, k , j, dp)), res);
         }
         dp[i][j] = res;
         return res;
+    }
+    
+    private int useTabulation(int [] values) {
+        int n = values.length;
+        int[][] dp = new int[n][n];
+        return 0;
     }
 }
