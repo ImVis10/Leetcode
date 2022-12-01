bd3ab0477d954aa26d4316338f835865cbaf3fc0
Elio <41447494+ImVis10@users.noreply.github.com>
2022-12-01 11:08:10 -0500

    Time: 11 ms (57.23%), Space: 45.6 MB (57.54%) - LeetHub

---
+++ 1513-number-of-substrings-with-only-1s/1513-number-of-substrings-with-only-1s.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int numSub(String s) {
+        int mod = (int) Math.pow(10, 9) + 7;
+        int res = 0, count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            count = s.charAt(i) == '1' ? count + 1 : 0; // increment count whenever a '1' is found.
+            res = (res + count) % mod; // update the no. of substrings traversed till now.
+        }
+        return res;
+    }
+}
