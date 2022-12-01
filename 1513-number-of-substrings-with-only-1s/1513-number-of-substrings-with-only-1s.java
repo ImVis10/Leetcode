6e4daa7d568dc353b51daad3a3cf4dc8d6ccb143
Elio <41447494+ImVis10@users.noreply.github.com>
2022-12-01 11:09:44 -0500

    Time: 6 ms (93.23%), Space: 42.3 MB (88.62%) - LeetHub

--- 1513-number-of-substrings-with-only-1s/1513-number-of-substrings-with-only-1s.java
+++ 1513-number-of-substrings-with-only-1s/1513-number-of-substrings-with-only-1s.java
@@ -3,7 +3,7 @@ class Solution {
         int mod = (int) Math.pow(10, 9) + 7;
         int res = 0, count = 0;
         for (int i = 0; i < s.length(); i++) {
-            count = s.charAt(i) == '1' ? count + 1 : 0; // increment count whenever a '1' is found.
+            count = s.charAt(i) == '1' ? count + 1 : 0; // increment count whenever a consecutive '1' is found.
             res = (res + count) % mod; // update the no. of substrings traversed till now.
         }
         return res;
