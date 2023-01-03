8acc9f6cd79126fe912119086787665260ce62b3
Elio <41447494+ImVis10@users.noreply.github.com>
2023-01-03 13:00:52 -0500

    Time: 76 ms (93.76%), Space: 19.7 MB (95.85%) - LeetHub

---
+++ 0015-3sum/0015-3sum.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        // same as twosum, here the target is zero and there can be more than one answer, we need to find all the unique triplets that sum to 0. Solution shouldn't contain duplicate triplets.
+        int target = 0;
+        vector<vector<int>> res;
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size(); i++) {
+            if (!(nums[i] > 0)) { // as the array is sorted, remaining values can't be summed to 0.
+                if (i == 0 || nums[i] != nums[i - 1]) {
+                    twoSum(nums, i, res, target);
+                }
+            }
+        }
+        return res;
+        
+    }
+    
+    void twoSum(vector<int> &nums, int i, vector<vector<int>> &res, int target) {
+        int left = i + 1, right = nums.size() - 1;
+        while (left < right) {
+            int sum = nums[left] + nums[right] + nums[i];
+            if (sum == target) {
+                res.push_back({nums[i], nums[left++], nums[right--]});
+                while (left < right && nums[left] == nums[left - 1]) {
+                    left++;
+                }
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+    }
+};
