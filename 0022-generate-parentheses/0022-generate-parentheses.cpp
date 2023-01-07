2cbdbc82a4426c18f7854f958883ea5df2c429d5
Elio <41447494+ImVis10@users.noreply.github.com>
2023-01-07 18:38:54 -0500

    Time: 0 ms (100.00%), Space: 13.8 MB (44.09%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        // no. of open parentheses  = n, no. of closing parentheses = n
+        // add open parentheses only when open < n
+        // add closing parentheses only when close < open
+        vector<string> res;
+        backtrack(0, 0, n, "", res);
+        return res;
+    }
+private:
+    void backtrack(int numOpen, int numClose, int n, string str, vector<string> & res) {
+        if (numOpen == numClose && numClose == n) {
+            res.push_back(str);
+            return;
+        }
+        
+        if (numClose < numOpen) {
+            backtrack(numOpen, numClose + 1, n, str + ')', res);
+        }
+        
+        if (numOpen < n) {
+            backtrack(numOpen + 1, numClose, n, str + '(', res);
+        }
+        
+
+    }
+};
