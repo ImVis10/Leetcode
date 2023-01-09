4fbdf057ef7c89681f671ff7474832edaccbfb32
Elio <41447494+ImVis10@users.noreply.github.com>
2023-01-08 22:27:47 -0500

    Time: 0 ms (100.00%), Space: 6.2 MB (74.97%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.cpp
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -1,33 +1,84 @@
+// class Solution {
+// public:
+//     bool backspaceCompare(string s, string t) {
+//         stack<char> stack1;
+//         stack<char> stack2;
+        
+//         pushToStack(s, stack1);
+//         pushToStack(t, stack2);
+        
+//         return getStringFromStack(stack1) == getStringFromStack(stack2);
+//     }
+
+// private:
+//     void pushToStack(string s, stack<char> &stack) {
+//         for(char ch : s) {
+//             if (!stack.empty() && ch == '#') {
+//                 stack.pop();
+//             } else if (ch != '#') {
+//                 stack.push(ch);
+//             }
+//         }
+//     }
+    
+//     string getStringFromStack(stack<char> &stack) {
+//         string str;
+//         while (!stack.empty()) {
+//             char ch = stack.top();
+//             str += ch;
+//             stack.pop();
+//         }
+//         return str;
+//     }
+// };
+
+// SOLVING FOLLOW-UP USING O(1) EXTRA SPACE
+// Got this from the solution section
+
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
-        stack<char> stack1;
-        stack<char> stack2;
+        int i = s.length() - 1, j = t.length() - 1; // iterate throught the string in reverse.
+        int skipS = 0, skipT = 0; // if we arrive at a backspace, the next non-backspace character is skipped.
         
-        pushToStack(s, stack1);
-        pushToStack(t, stack2);
+        // If a character isn't skipped, it is a part of the final result
         
-        return getStringFromStack(stack1) == getStringFromStack(stack2);
-    }
-
-private:
-    void pushToStack(string s, stack<char> &stack) {
-        for(char ch : s) {
-            if (!stack.empty() && ch == '#') {
-                stack.pop();
-            } else if (ch != '#') {
-                stack.push(ch);
+        while (i >= 0 || j >= 0) {
+            while (i >= 0) {
+                if (s[i] == '#') {
+                    skipS++;
+                    i--;
+                } else if (skipS > 0) {
+                    skipS--;
+                    i--;
+                } else {
+                    break;
+                }
             }
+            
+            while (j >= 0) {
+                if (t[j] == '#') {
+                    skipT++;
+                    j--;
+                } else if (skipT > 0) {
+                    skipT--;
+                    j--;
+                } else {
+                    break;
+                }
+            }
+            
+            if ((i >= 0) != (j >= 0)) { // if the result is not of the same length
+                return false;
+            }
+            
+            if (i >= 0 && j >= 0 && s[i] != t[j]) { // if the contents of the result are different
+                return false;
+            }
+            
+            i--;
+            j--;
         }
-    }
-    
-    string getStringFromStack(stack<char> &stack) {
-        string str;
-        while (!stack.empty()) {
-            char ch = stack.top();
-            str += ch;
-            stack.pop();
-        }
-        return str;
+        return true;
     }
 };
