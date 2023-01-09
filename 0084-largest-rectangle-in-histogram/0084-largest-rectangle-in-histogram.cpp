de29fa754c27c881a5614d48fffce7ae2c46074e
Elio <41447494+ImVis10@users.noreply.github.com>
2023-01-08 22:38:00 -0500

    Time: 177 ms (91.06%), Space: 79.6 MB (60.32%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -5,18 +5,18 @@ public:
         stack<pair<int, int>> stack;
         
         for (int i = 0; i < heights.size(); i++) {
-            int start = i;
+            int start = i; // start index of the histogram under consideration
             while (!stack.empty() && stack.top().second > heights[i]) { // Top of the stack always = highest bar until that moment.
                 int index = stack.top().first; // index of the previous higher bar
                 int height = stack.top().second; // height of the previous higher bar
-                stack.pop();
+                stack.pop(); // pop when height is less than the previous
                 maxArea = max(maxArea, height * (i - index)); // width = i - index
-                start = index; // as the bar at height[i] can be extended back until this index with height = heights[i]
+                start = index; // as the bar at height[i] can be extended back until this index where height <= heights at previous indices
             }
             stack.push({start, heights[i]});
         }
         
-        while (!stack.empty()) {
+        while (!stack.empty()) { // for the remaining elements in stack, we check the area for each of them
             int height = stack.top().second;
             int width = heights.size() - stack.top().first;
             stack.pop();
