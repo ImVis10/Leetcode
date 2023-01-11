b76b80d145aeb679b51809edbd3c0818e78f6809
Elio <41447494+ImVis10@users.noreply.github.com>
2023-01-11 15:26:56 -0500

    Time: 552 ms (81.94%), Space: 102.4 MB (14.63%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     int findMinArrowShots(vector<vector<int>>& points) {
-        
-        // sort the points in the increasing order of xend
+        // Can make use of greedy approach
+        // try to shoot an an arrow in such a manner that it bursts as many balloons as it can
+        // to achieve that balloons must be sorted in the increasing order of their ending positions
         
         sort(points.begin(), points.end(), [](auto const &a, auto const &b) {
             return a[1] < b[1];
         });
         
         int numArrows = 1;
-        int prevEnd = points[0][1];
+        int currStart, currEnd, prevEnd = points[0][1];
         
-        for(int i = 0; i < points.size(); i++) {
-            int xStart = points[i][0];
-            int xEnd = points[i][1];
+        for (auto point : points) {
+            currStart = point[0];
+            currEnd = point[1];
             
-            if (xStart > prevEnd) {
+            if (currStart > prevEnd) { // if the current balloon starts after the end of the previous one, an extra arrow is needed.
+                prevEnd = currEnd;
                 numArrows++;
-                prevEnd = xEnd;
             }
         }
         return numArrows;
