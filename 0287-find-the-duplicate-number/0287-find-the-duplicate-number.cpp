b17c79e8782a784d54fa799fa212d054e9f21f26
Elio <41447494+ImVis10@users.noreply.github.com>
2023-01-11 16:45:55 -0500

    Time: 120 ms (83.67%), Space: 61.2 MB (93.12%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
+        // Make a linked list sort of data structure based on the numbers in nums
+        // element points to its index
+        // The linked list sort of DS should contain the elements of nums and each element 'i' should point to the the element at index = i
+        // Then apply Floyd's cycle detection algo on the linked list
+        // Origin of cycle is the result i.e. Duplicate
+        // More at: https://imgur.com/a/WJfYYmo
         int slow = 0, fast = 0;
-        do {
+        while (true) {
             slow = nums[slow];
             fast = nums[nums[fast]];
             if (slow == fast) {
                 break;
             }
         }
-        while (true);
         
         int secondSlow = 0;
         
-        do {
+        while (true) {
             slow = nums[slow];
             secondSlow = nums[secondSlow];
             if (slow == secondSlow) {
                 return slow;
             }
         }
-        while (true);
     }
 };
