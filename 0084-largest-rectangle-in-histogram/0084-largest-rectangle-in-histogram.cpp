552cae7b4a15b76fe5077824c3c646d5bb7a8975
Elio <41447494+ImVis10@users.noreply.github.com>
2023-01-11 21:17:58 -0500

    Time: 161 ms (96.04%), Space: 79.7 MB (60.69%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
+        stack<pair<int, int>> stack;
         int maxArea = 0;
-        stack<pair<int, int>> stack; // starting index of histogram, height of the histogram
         
-        for (int i = 0; i < heights.size(); i++) {
-            int start = i; // start index of the histogram under consideration
-            while (!stack.empty() && stack.top().second > heights[i]) { // Top of the stack always = highest bar until that moment.
-                int index = stack.top().first; // index of the previous higher bar
-                int height = stack.top().second; // height of the previous higher bar
-                stack.pop(); // pop when height is less than the previous
-                maxArea = max(maxArea, height * (i - index)); // width = i - index
-                start = index; // as the bar at height[i] can be extended back until this index where height <= heights at previous indices
+        for(int i = 0; i < heights.size(); i++) {
+            int startIndex = i;
+            while (!stack.empty() && stack.top().second > heights[i]) {
+                int index = stack.top().first;
+                int height = stack.top().second;
+                stack.pop();
+                startIndex = index; // starting index of the current bar can be extended back as its height is less than its previous one
+                maxArea = max(maxArea, height * (i - index));
             }
-            stack.push({start, heights[i]});
+            stack.push({startIndex, heights[i]});
         }
         
-        while (!stack.empty()) { // for the remaining elements in stack, we check the area for each of them
-            int height = stack.top().second;
+        while (!stack.empty()) {
             int width = heights.size() - stack.top().first;
+            int height = stack.top().second;
             stack.pop();
             maxArea = max(maxArea, height * width);
         }
-        
         return maxArea;
     }
 };
