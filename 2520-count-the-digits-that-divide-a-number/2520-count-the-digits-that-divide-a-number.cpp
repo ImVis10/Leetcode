d0697f50a5ad8826e16da09c4c72f2b8b1a69eb1
Elio <41447494+ImVis10@users.noreply.github.com>
2023-01-12 13:57:07 -0500

    Time: 3 ms (41.61%), Space: 5.9 MB (59.94%) - LeetHub

--- 2520-count-the-digits-that-divide-a-number/2520-count-the-digits-that-divide-a-number.cpp
+++ 2520-count-the-digits-that-divide-a-number/2520-count-the-digits-that-divide-a-number.cpp
@@ -1,13 +1,19 @@
 class Solution {
 public:
     int countDigits(int num) {
-        int res = 0;
-        int temp = num;
-        while(temp > 0) {
-            int digit = temp % 10;
-            temp /= 10;
-            res += (num % digit == 0);
-        }
-        return res;
+        // int res = 0;
+        // int temp = num;
+        // while(temp > 0) {
+        //     int digit = temp % 10;
+        //     temp /= 10;
+        //     res += (num % digit == 0);
+        // }
+        // return res;
+        
+        // TOOK THIS SOLUTION FROM VORTUBAC AS ITS NEATER ACCORDING TO ME
+        auto str = to_string(num);
+        return accumulate(begin(str), end(str), 0, [&](int res, char ch) {
+            return res + (num % (ch - '0') == 0);
+        });
     }
 };
