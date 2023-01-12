88109150c9aaef27cc5b8be1f9ec80e96b2f2990
Elio <41447494+ImVis10@users.noreply.github.com>
2023-01-11 22:48:53 -0500

    Time: 55 ms (93.07%), Space: 33.7 MB (64.78%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
@@ -1,18 +1,37 @@
 class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
-        vector<int> res;
+//         vector<int> res;
         
+//         int n = nums.size();
+        
+//         set<int> set;
+        
+//         for(int num : nums) {
+//             set.insert(num);
+//         }
+        
+//         for (int i = 1; i <= n; i++) {
+//             if(!set.count(i)) {
+//                 res.push_back(i);
+//             }
+//         }
+//         return res;
+        // WITHOUT USING EXTRA SPACE
+        // IDEA: MUTATE THE INPUT ARRAY TO INDICATE WHETHER THE NUMBER IS PRESENT IN THE ARRAY
+        // SINCE ALL THE ELEMENTS ARE IN THE RANGE [1, N]
+        // -VE SIGN INDICATES THAT THE NUMBER IS PRESENT IN THE ARRAY
+        vector<int> res;
         int n = nums.size();
         
-        set<int> set;
-        
-        for(int num : nums) {
-            set.insert(num);
+        for (int  i = 0; i < n; i++) {
+            if (nums[abs(nums[i]) - 1] > 0) {
+                nums[abs(nums[i]) - 1] *= - 1;
+            }
         }
         
-        for (int i = 1; i <= n; i++) {
-            if(!set.count(i)) {
+        for (int  i = 1; i <= n; i++) {
+            if (nums[i - 1] > 0) {
                 res.push_back(i);
             }
         }
