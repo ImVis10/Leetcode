8bc271080f1a078b82a0c7df19bbb033444c7847
Elio <41447494+ImVis10@users.noreply.github.com>
2023-01-13 14:56:05 -0500

    Time: 395 ms (90.78%), Space: 177.8 MB (77.18%) - LeetHub

--- 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.cpp
+++ 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.cpp
@@ -1,43 +1,39 @@
 class Solution {
+    // TOOK SOLUTION FROM SOLUTION SECTION
+
 public:
-    int dfs(int currentNode, vector<vector<int>>& children, string& s, int& longestPath) {
-        // Longest and second longest chains starting from currentNode (does not count the
-        // currentNode itself).
-        int longestChain = 0, secondLongestChain = 0;
-        for (int child : children[currentNode]) {
-            // Get the number of nodes in the longest path in the subtree of child,
-            // including the child.
-            int longestChainStartingFromChild = dfs(child, children, s, longestPath);
-            // We won't move to the child if it has the same character as the currentNode.
-            if (s[currentNode] == s[child]) {
-                continue;
-            }
-            // Modify the longestChain and secondLongestChain if longestChainStartingFromChild
-            // is bigger.
-            if (longestChainStartingFromChild > longestChain) {
-                secondLongestChain = longestChain;
-                longestChain = longestChainStartingFromChild;
-            } else if (longestChainStartingFromChild > secondLongestChain) {
-                secondLongestChain = longestChainStartingFromChild;
-            }
-        }
-
-        // Add "1" for the node itself.
-        longestPath = max(longestPath, longestChain + secondLongestChain + 1);
-        return longestChain + 1;
-    }
-
     int longestPath(vector<int>& parent, string s) {
         int n = parent.size();
         vector<vector<int>> children(n);
-        // Start from node 1, since root node 0 does not have a parent.
         for (int i = 1; i < n; i++) {
             children[parent[i]].push_back(i);
         }
-
+        
         int longestPath = 1;
         dfs(0, children, s, longestPath);
-
+        
         return longestPath;
+    }
+    
+private:
+    int dfs(int node, vector<vector<int>>& children, string& s, int& longestPath) {
+        int longestChain = 0, secondLongestChain = 0; // longest and second longest chains across all the children of node
+        for (int child : children[node]) {
+            int longestPathStartingFromChild = dfs(child, children, s, longestPath);
+            
+            if (s[child] == s[node]) { // if adjacent characters are the same, don't include them in the chain. Just continue
+                continue;
+            }
+            
+            if (longestPathStartingFromChild > longestChain) {
+                secondLongestChain = longestChain;
+                longestChain = longestPathStartingFromChild;
+            } else if (longestPathStartingFromChild > secondLongestChain) {
+                secondLongestChain = longestPathStartingFromChild;
+            }
+        }
+        
+        longestPath = max(longestPath, 1 + longestChain + secondLongestChain);
+        return 1 + longestChain;
     }
 };
