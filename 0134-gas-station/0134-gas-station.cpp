5affe0080c591de7a59687f9d1b412657612db9d
Elio <41447494+ImVis10@users.noreply.github.com>
2023-01-13 23:09:02 -0500

    Time: 117 ms (8.44%), Space: 89 MB (5.04%) - LeetHub

--- 0134-gas-station/0134-gas-station.cpp
+++ 0134-gas-station/0134-gas-station.cpp
@@ -15,11 +15,22 @@ public:
         for (int i = 0; i < n; i++) {
             total += gas[i] - cost[i];
             
+/**When the total stays +ve, we don't do anything to the start point, our start pointer points to the first index when our total became positive.
+
+suppose we start from X(-ve)--->Y(-ve)--->A(+ve)---->B(+ve)---->C(+ve), where C is the end of the array, our start(which is also the ans) would be A.
+
+Why not B? why not C?
+It is because we moved from A to B with some +ve value or atleast 0, whereas if we started from B we would have had only the value of B so earlier point added some value to our total, so its more favorable to help us reach the ans, hence earliest point is always better.
+
+Why we just stop at point C and don t complete the cycle and check?
+It is because if we arrived till here it means that there is an answer and it is unique, so we will always choose the most favorable answer which is also the fundamental idea of Greedy Algorithims.**/
+            
             if (total < 0) {
                 total = 0;
                 start = i + 1;
             }
         }
+        
         return start;
     }
 };
