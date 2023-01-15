9754cb0b607c4b987b71e6db96c8830750dcbdf2
Elio <41447494+ImVis10@users.noreply.github.com>
2023-01-15 10:31:06 -0500

    Time: 6 ms (83.77%), Space: 13 MB (63.27%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int profit = 0;
-        
+        // we have to buy at local minima and sell at local maxima
         for (int i = 1;  i < prices.size(); i++) {
             // we buy before and sell after
-            if (prices[i] > prices[i - 1]) { // if selling prices is greater than buying price
+            if (prices[i] > prices[i - 1]) { // if selling price is greater than buying price
                 profit += (prices[i] - prices[i-1]);
             }
         }
