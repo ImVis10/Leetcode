34b94cecd9b14cd0df01ab8935045aa2ddb50060
Elio <41447494+ImVis10@users.noreply.github.com>
2023-01-16 11:08:21 -0500

    Time: 15 ms (43.91%), Space: 59.7 MB (53.96%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -8,32 +8,51 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// class Solution {
+//     public boolean isPalindrome(ListNode head) {
+        
+//         if(head == null) return false;
+        
+//         Stack<Integer> stack = new Stack<>();
+//         boolean isPalindrome = false;
+        
+//         ListNode node = head;
+//         while (node != null) {
+//             stack.push(node.val);
+//             node = node.next;
+//         }
+        
+//         node = head;
+        
+//         while (node != null) {
+//             if (stack.pop() == node.val) {
+//                 isPalindrome = true;
+//             } else {
+//                 isPalindrome = false;
+//                 break;
+//             }
+//             node = node.next;
+//         }
+        
+//         return isPalindrome;
+//     }
+// }
+
 class Solution {
+
+    private ListNode frontPointer;
+
+    private boolean recursivelyCheck(ListNode currentNode) {
+        if (currentNode != null) {
+            if (!recursivelyCheck(currentNode.next)) return false;
+            if (currentNode.val != frontPointer.val) return false;
+            frontPointer = frontPointer.next;
+        }
+        return true;
+    }
+
     public boolean isPalindrome(ListNode head) {
-        
-        if(head == null) return false;
-        
-        Stack<Integer> stack = new Stack<>();
-        boolean isPalindrome = false;
-        
-        ListNode node = head;
-        while (node != null) {
-            stack.push(node.val);
-            node = node.next;
-        }
-        
-        node = head;
-        
-        while (node != null) {
-            if (stack.pop() == node.val) {
-                isPalindrome = true;
-            } else {
-                isPalindrome = false;
-                break;
-            }
-            node = node.next;
-        }
-        
-        return isPalindrome;
+        frontPointer = head;
+        return recursivelyCheck(head);
     }
 }
