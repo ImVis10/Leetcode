db2eae12b4d039356a777f822db918a2c1f9c076
Elio <41447494+ImVis10@users.noreply.github.com>
2023-01-16 18:51:08 -0500

    Time: 31 ms (62.66%), Space: 14.7 MB (30.06%) - LeetHub

--- 0929-unique-email-addresses/0929-unique-email-addresses.cpp
+++ 0929-unique-email-addresses/0929-unique-email-addresses.cpp
@@ -1,14 +1,33 @@
 class Solution {
 public:
     int numUniqueEmails(vector<string>& emails) {
+        // set<string> uniqueEmails;
+        // for (string email : emails) {
+        //     string localName = email.substr(0, email.find('@'));
+        //     localName = localName.substr(0, email.find('+')); // find returns first occurrence of '+'
+        //     localName = regex_replace(localName, regex("\\."), "");
+        //     string domainName = email.substr(email.find('@') + 1, email.length());
+        //     email = localName + '@' + domainName;
+        //     uniqueEmails.insert(email);
+        // }
+        // return uniqueEmails.size();
+        
         set<string> uniqueEmails;
+        
         for (string email : emails) {
-            string localName = email.substr(0, email.find('@'));
-            localName = localName.substr(0, email.find('+')); // find returns first occurrence of '+'
-            localName = regex_replace(localName, regex("\\."), "");
-            string domainName = email.substr(email.find('@') + 1, email.length());
-            email = localName + '@' + domainName;
-            uniqueEmails.insert(email);
+            string cleanEmail;
+            for (char ch : email) {
+                if (ch == '+' || ch == '@') {
+                    break;
+                }
+                if (ch == '.') {
+                    continue;
+                }
+                cleanEmail += ch;
+            }
+            cleanEmail += email.substr(email.find('@'));
+            cout << cleanEmail << endl;
+            uniqueEmails.insert(cleanEmail);
         }
         return uniqueEmails.size();
     }
