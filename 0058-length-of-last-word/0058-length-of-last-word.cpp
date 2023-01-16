38d13fbba8e14e50858558c1ccf2ebf9a4e68bed
Elio <41447494+ImVis10@users.noreply.github.com>
2023-01-15 19:23:12 -0500

    Time: 4 ms (45.63%), Space: 6.5 MB (91.16%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     int lengthOfLastWord(string s) {
         int len = s.length();
-        bool flag = false;
+        bool spacesAfterLastWord = true;
         int res = 0;
         
         for (int i = len - 1; i >= 0; i--) {
-            if (flag && s[i] == ' ') {
-                return res;
-            } else if (s[i] == ' ') {
+            if (spacesAfterLastWord && s[i] == ' ') {
                 continue;
-            } else {
+            } else if (s[i] != ' ') {
                 res++;
-                flag = true;
+                spacesAfterLastWord = false;
+            } else {
+                break;
             }
         }
         return res;
