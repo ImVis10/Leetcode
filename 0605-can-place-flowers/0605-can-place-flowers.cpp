2c272bcc7650338a24e2d4b741d02df8442a4c51
Elio <41447494+ImVis10@users.noreply.github.com>
2023-01-15 20:40:29 -0500

    Time: 25 ms (54.60%), Space: 20.3 MB (28.08%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -5,15 +5,28 @@ public:
         // if both are empty, a flower can be planted
         // if not, a flower can't be planted
         
-        flowerbed.insert(flowerbed.begin(), 0);
-        flowerbed.push_back(0);
+//         flowerbed.insert(flowerbed.begin(), 0);
+//         flowerbed.push_back(0);
         
-        for (int i = 1; i < flowerbed.size() - 1; i++) {
-            if (flowerbed[i - 1] == 0 && flowerbed[i] == 0 && flowerbed[i + 1] == 0) {
-                flowerbed[i] = 1;
-                n--;
+//         for (int i = 1; i < flowerbed.size() - 1; i++) {
+//             if (flowerbed[i - 1] == 0 && flowerbed[i] == 0 && flowerbed[i + 1] == 0) {
+//                 flowerbed[i] = 1;
+//                 n--;
+//             }
+//         }
+//         return n <= 0;
+        
+        int empty = flowerbed[0] ? 0 : 1;
+        //  we can place at most (c - 1) / 2 flowers into the gap, where c is a number of continuous 0 in the gap. With two exceptions, when first or last position is 0 we need extra +1.
+        for (int flower : flowerbed) {
+            if (flower) {
+                n -= ((empty - 1) / 2);
+                empty = 0;
+            } else {
+                empty++;
             }
         }
+        n -= (empty / 2);
         return n <= 0;
     }
 };
