c6e58eb51f105f6e2f12ba7c1a9334e2d0cc691a
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-05-01 11:39:51 -0400

    Time: 5 ms (23.02%), Space: 7.1 MB (84.06%) - LeetHub

---
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    double average(vector<int>& salary) {
+        double res = 0;
+        
+        sort(salary.begin(), salary.end());
+        
+        for (int i = 1; i < salary.size() - 1; i++) {
+            res += salary[i];
+        }
+        
+        return res / (salary.size() - 2);
+    }
+};
