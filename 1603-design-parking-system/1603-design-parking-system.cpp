5f6f962ce934daf218ae3a2dc1d36a0e6f09f6f4
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-05-29 12:31:09 -0400

    Time: 66 ms (43.21%), Space: 33.2 MB (28.39%) - LeetHub

--- 1603-design-parking-system/1603-design-parking-system.cpp
+++ 1603-design-parking-system/1603-design-parking-system.cpp
@@ -1,40 +1,21 @@
 class ParkingSystem {
 public:
     ParkingSystem(int big, int medium, int small) {
-        big1 = big;
-        medium1 = medium;
-        small1 = small;
+        parkingSlotCount[1] = big;
+        parkingSlotCount[2] = medium;
+        parkingSlotCount[3] = small;
     }
     
     bool addCar(int carType) {
-        if (carType == 1) {
-            if (big1 == 0) {
-                return false;
-            } else {
-                big1 -= 1;
-                return true;
-            }
-        } else if (carType == 2) {
-            if (medium1 == 0) {
-                return false;
-            } else {
-                medium1 -= 1;
-                return true;
-            }
-        } else {
-            if (small1 == 0) {
-                return false;
-            } else {
-                small1 -= 1;
-                return true;
-            }
+        if (parkingSlotCount[carType] == 0) {
+            return false;
         }
+        parkingSlotCount[carType] -= 1;
+        return true;
     }
     
 private:
-    int big1;
-    int medium1;
-    int small1;
+    map<int, int> parkingSlotCount;
 };
 
 /**
