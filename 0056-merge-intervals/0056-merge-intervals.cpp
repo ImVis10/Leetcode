8140145d1ddf1d31eef9b6ad4493da7a27848980
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-05-29 23:25:11 -0400

    Time: 41 ms (66.22%), Space: 18.9 MB (81.71%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -6,22 +6,31 @@ public:
         int n = intervals.size();
         sort(intervals.begin(), intervals.end());
         
+//         for (int i = 0; i < n; i++) {
+//             int start = intervals[i][0];
+//             int end = intervals[i][1];
+            
+//             if (!res.empty() && end <= res.back()[1]) {
+//                 continue;
+//             }
+            
+//             for (int j = i + 1; j < n; j++) {
+//                 if (intervals[j][0] <= end) {
+//                     end = max(end, intervals[j][1]);
+//                 } else {
+//                     break;
+//                 }
+//             }
+//             res.push_back({start, end});
+//         }
+        
+        // OPTIMAL APPROACH
         for (int i = 0; i < n; i++) {
-            int start = intervals[i][0];
-            int end = intervals[i][1];
-            
-            if (!res.empty() && end <= res.back()[1]) {
-                continue;
+            if (res.empty() || intervals[i][0] > res.back()[1]) {
+                res.push_back(intervals[i]);
+            } else {
+                res.back()[1] = max(intervals[i][1], res.back()[1]);
             }
-            
-            for (int j = i + 1; j < n; j++) {
-                if (intervals[j][0] <= end) {
-                    end = max(end, intervals[j][1]);
-                } else {
-                    break;
-                }
-            }
-            res.push_back({start, end});
         }
         return res;
     }
