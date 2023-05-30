cbf8ce61ebe58096375b300500be7fb6dc395f64
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-05-30 13:02:35 -0400

    Time: 2952 ms (27.70%), Space: 26.2 MB (25.84%) - LeetHub

--- 1799-maximize-score-after-n-operations/1799-maximize-score-after-n-operations.py
+++ 1799-maximize-score-after-n-operations/1799-maximize-score-after-n-operations.py
@@ -9,6 +9,8 @@ class Solution:
             for i in range(len(nums)):
                 for j in range(i + 1, len(nums)):
                     if (1 << i) & mask or (1 << j) & mask: # AND to read
+                        # 00'0'00 -- check if the the 3rd bit is already used i.e. i = 3
+                        # left shift 1 until the 3rd bit and do a bitwise AND
                         continue;
                     newMask = mask | (1 << i) | (1 << j) # OR to write
                     score = op * math.gcd(nums[i], nums[j])
