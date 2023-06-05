ed847f7f55e3b273904243b2ce5077d154e357c0
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-06-05 15:50:32 -0400

    Time: 15 ms (65.96%), Space: 5.9 MB (49.56%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,23 +1,37 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if (x < 0) {
+//         if (x < 0) {
+//             return false;
+//         }
+        
+//         int rev = 0;
+//         int x1 = x;
+        
+//         while (x != 0) {
+//             if (rev > INT_MAX / 10 || rev < INT_MIN / 10) {
+//                 return false;
+//             }
+//             int rem = x % 10;
+//             int temp = rev * 10 + rem;
+//             rev = temp;
+//             x /= 10;
+//         }
+        
+//         return x1 == rev;
+        
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
             return false;
         }
-        
+            
         int rev = 0;
-        int x1 = x;
-        
-        while (x != 0) {
-            if (rev > INT_MAX / 10 || rev < INT_MIN / 10) {
-                return false;
-            }
+            
+        while (x > rev) {
             int rem = x % 10;
             int temp = rev * 10 + rem;
             rev = temp;
             x /= 10;
         }
-        
-        return x1 == rev;
+        return x == rev || x == rev / 10; // second condition if x is an odd-length number
     }
 };
