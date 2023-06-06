5ae317207ceec6ea09466c507c77b98e902a39db
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-06-06 17:43:43 -0400

    Time: 26 ms (57.03%), Space: 19.2 MB (78.49%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -8,8 +8,7 @@ public:
         for (i = 0; i < n; i ++) {
             if (nums[i] != 0) {
                 if (j < i) {
-                    nums[j] = nums[i];
-                    nums[i] = 0;
+                    swap(nums[j], nums[i]);
                 }
                 j++;
              }
