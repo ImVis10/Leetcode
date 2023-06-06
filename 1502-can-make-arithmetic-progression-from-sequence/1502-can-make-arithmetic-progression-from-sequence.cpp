01e9afa0ab5942d1e3ee4b629ab360271aef5f0c
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-06-06 01:36:33 -0400

    Time: 4 ms (74.54%), Space: 9.1 MB (66.22%) - LeetHub

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.cpp
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.cpp
@@ -1,15 +1,43 @@
 class Solution {
 public:
     bool canMakeArithmeticProgression(vector<int>& arr) {
-        sort(arr.begin(), arr.end());
+//         sort(arr.begin(), arr.end());
         
-        int diff = arr[1] - arr[0];
+//         int diff = arr[1] - arr[0];
         
+//         int n = arr.size();
+        
+//         for (int i = 2; i < n; i++) {
+//             if (diff != arr[i] - arr[i - 1]) {
+//                 return false;
+//             }
+//         }
+//         return true;
+        
+        //  EFFICIENT APPROACH
+        int minVal = *min_element(arr.begin(), arr.end());
+        int maxVal = *max_element(arr.begin(), arr.end());
+         
         int n = arr.size();
         
-        for (int i = 2; i < n; i++) {
-            if (diff != arr[i] - arr[i - 1]) {
+        if ((maxVal - minVal) % (n - 1) != 0)
+            return false;
+        
+        int diff = (maxVal - minVal) / (n - 1);
+        
+        int i = 0;
+        
+        while (i < n) {
+            if (arr[i] == (i * diff) + minVal) {
+                i++;
+            } else if ((arr[i] - minVal) % diff != 0) {
                 return false;
+            } else {
+                int idealPos = (arr[i] - minVal) / diff;
+                if (arr[i] == arr[idealPos]) { // for duplicate elements
+                    return false;
+                }
+                swap(arr[i], arr[idealPos]);
             }
         }
         return true;
