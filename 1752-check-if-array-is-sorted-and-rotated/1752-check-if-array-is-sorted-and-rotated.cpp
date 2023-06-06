ba0163d84af75f6fc4c1cb55913ffe229bf05a55
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-06-06 16:18:36 -0400

    Time: 3 ms (50.58%), Space: 8.4 MB (41.60%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -12,7 +12,7 @@ public:
         int maxAfterBreakPoint = -1;
         
         for (int i = 0; i < n - 1; i++) {
-            if (nums[i] > nums[i + 1]) {
+            if (nums[i] > nums[i + 1]) { // dipping point
                 flag = 0;
                 maxBeforeBreakPoint = nums[i];
                 breakPoint = i + 1;
@@ -20,19 +20,19 @@ public:
             }
         }
         
-        if (flag == 1) {
+        if (flag == 1) { // if the array is already sorted
             return true;
         }
         
         for (int i = breakPoint; i < n - 1; i++) {
             if (nums[i] <= nums[i + 1]) {
                 maxAfterBreakPoint = nums[i + 1];
-            } else {
+            } else { // if the sub-array after the dipping point is not sorted
                 return false;
             }
         }
         
-        if (breakPoint == n - 1) {
+        if (breakPoint == n - 1) { // if the dipping point is at the last element of the array
             maxAfterBreakPoint = nums[breakPoint];
         }
         return maxAfterBreakPoint <= minBeforeBreakPoint;
