b5173d6b5fc78c40a8f5512d3f137b7ea55c3231
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-06-07 13:59:26 -0400

    Time: 3 ms (85.15%), Space: 9.4 MB (66.64%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,40 +1,38 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        // can use binary search on each of the rows
-        // each row --> O(log n), m rows --> m log n
-        // But solution should be found in O(log mn) --> log m + log n
-        // So first figure out which row the target is in O(log m) using binary search
-        // Then do another binary search on the target row 
-        int numRows = matrix.size(), numCols = matrix[0].size();
+        // each row is sorted in non decreasing order
+        // so can do binary search, to find out the row
         
+        int numRows = matrix.size(), numCols = matrix[0].size();
         int topRow = 0, bottomRow = numRows - 1;
         int currRow = 0;
         
+        // finding out the row
         while (topRow <= bottomRow) {
             currRow = topRow + (bottomRow - topRow) / 2;
-            if (matrix[currRow][0] > target) {
-                bottomRow = currRow - 1; 
-            } else if (matrix[currRow][numCols - 1] < target) {
+            if (target > matrix[currRow][numCols - 1]) {
                 topRow = currRow + 1;
-            } else {
+            } else if (target < matrix[currRow][0]) {
+                bottomRow = currRow - 1;
+            } else { // this might be the row
                 break;
             }
         }
         
-        if (topRow > bottomRow) return false;
         
+        // check if the element is present in the row
         int low = 0, high = numCols - 1;
-        int targetRow = currRow; 
+        int targetRow = currRow;
         
         while (low <= high) {
             int mid = low + (high - low) / 2;
-            if (matrix[targetRow][mid] > target) {
-                high = mid - 1;
-            } else if (matrix[targetRow][mid] < target) {
+            if (target == matrix[targetRow][mid]) {
+                return true;
+            } else if (target > matrix[targetRow][mid]) {
                 low = mid + 1;
             } else {
-                return true;
+                high = mid - 1;
             }
         }
         return false;
