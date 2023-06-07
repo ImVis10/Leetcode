556e82d25ab159e9b35c53f328e8d3862ee6a09a
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-06-07 18:34:13 -0400

    Time: 11 ms (83.52%), Space: 11.1 MB (14.82%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,17 +2,13 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         map<int, int> map;
-        vector<int> res;
         for (int i = 0; i < nums.size(); i++) {
             int rem = target - nums[i];
-            if (map.find(rem) == map.end()) {
-                map[nums[i]] = i;
-            } else {
-                res.push_back(map[rem]);
-                res.push_back(i);
-                return res;
+            if (map.find(rem) != map.end()) {
+                return {map[rem], i};
             }
+            map[nums[i]] = i;
         }
-        return res;
+        return {};
     }
 };
