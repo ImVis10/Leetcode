39e5feeb7f30101b1315712bf24a2b39577d547b
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-06-07 13:43:07 -0400

    Time: 20 ms (71.78%), Space: 16.8 MB (73.86%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -2,8 +2,9 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int res = 0;
+        
         for (int num : nums) {
-            res = res ^ num;
+            res = res ^ num; // a xor a  = 0, 0 xor a = a, 
         }
         return res;
     }
