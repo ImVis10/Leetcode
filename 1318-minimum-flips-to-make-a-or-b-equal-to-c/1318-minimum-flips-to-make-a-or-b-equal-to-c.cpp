2cc64db43c27e1748401ac71dd887d4a6c6841a6
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-06-07 20:42:30 -0400

    Time: 0 ms (100.00%), Space: 6.1 MB (16.65%) - LeetHub

--- 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
@@ -3,7 +3,9 @@ public:
     int minFlips(int a, int b, int c) {
         // to flip kth bit ==> num ^ (1 << k)
         int numFlips = 0;
-        for (int i = 0; i < 32; i++) {
+        int n = max({getNumBits(a), getNumBits(b), getNumBits(c)});
+        
+        for (int i = 0; i < n; i++) {
             int getBitA = bool(a & (1 << i));
             int getBitB = bool(b & (1 << i));
             int getBitC = bool(c & (1 << i));
@@ -19,5 +21,14 @@ public:
             }
         }
         return numFlips;
+    }
+private:
+    int getNumBits(int x) {
+        int cnt = 0;
+        while (x) {
+            x = x >> 1;
+            cnt++;
+        }
+        return cnt;
     }
 };
