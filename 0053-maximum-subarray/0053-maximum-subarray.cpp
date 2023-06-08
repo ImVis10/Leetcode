bed868d6bac1cdc2f5620ebf6f7819848e1327f3
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-06-08 17:47:19 -0400

    Time: 130 ms (30.84%), Space: 67.6 MB (87.34%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int max_sum = INT_MIN;
+        // Kadane's algorithm
+        // Don't move forward with negative sum coz its better to start with new one as our objective is to maximize the sum of the subarray 
+        int maxSum = INT_MIN;
         int sum = 0;
-
+        
         for (int i = 0; i < nums.size(); i++) {
             sum += nums[i];
-            max_sum = max(sum, max_sum);
-
+            maxSum = max(sum, maxSum);
             if (sum < 0) {
                 sum = 0;
             }
         }
-
-        return max_sum;
+        return maxSum;
     }
 };
