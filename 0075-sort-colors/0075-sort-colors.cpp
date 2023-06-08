474bd6bab76775c102d83b3565c4c5dd8553c39e
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-06-08 17:41:34 -0400

    Time: 0 ms (100.00%), Space: 8.2 MB (49.68%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -3,7 +3,7 @@ public:
     void sortColors(vector<int>& nums) {
         // Dutch National Flag problem
         int low = 0, mid = 0, high = nums.size() - 1;
-        
+        // https://imgur.com/a/qowQCvD
         while (mid <= high) { // as long as there is unsorted section
             if (nums[mid] == 0) {
                 swap(nums[low], nums[mid]);
