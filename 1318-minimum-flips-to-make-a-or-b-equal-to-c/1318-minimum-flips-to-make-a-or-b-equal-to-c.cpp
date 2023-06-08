818530307c6e0ae8489a1dff79d47b10b4376cd2
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-06-07 20:30:22 -0400

    Time: 3 ms (40.27%), Space: 6.1 MB (16.65%) - LeetHub

--- 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
@@ -8,13 +8,13 @@ public:
             int getBitB = bool(b & (1 << i));
             int getBitC = bool(c & (1 << i));
             if (getBitC == 0) {
-                if (getBitC != getBitA) {
+                if (getBitC != getBitA) { // both a and b bits should be 0 for c bit to be 0 as we're doing an OR
                     numFlips++;
                 }
                 if (getBitC != getBitB) {
                     numFlips++;
                 }
-            } else if (getBitC != getBitA && getBitC != getBitB) {
+            } else if (getBitC != getBitA && getBitC != getBitB) { // if c bit == 1, then even if both a and b bits are 0, flipping one of them suffices
                     numFlips++;
             }
         }
