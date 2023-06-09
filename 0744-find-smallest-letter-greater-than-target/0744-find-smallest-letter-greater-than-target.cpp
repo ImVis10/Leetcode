a2bbd7a59c9cab71d18a4fdd261056ab611bbea2
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-06-09 12:28:30 -0400

    Time: 4 ms (99.78%), Space: 15.9 MB (32.10%) - LeetHub

--- 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
@@ -1,11 +1,26 @@
 class Solution {
 public:
     char nextGreatestLetter(vector<char>& letters, char target) {
+        // char res = letters[0];
+        // for (char letter : letters) {
+        //     if (letter > target) {
+        //         res = letter;
+        //         break;
+        //     }
+        // }
+        // return res;
+        
+        // The above approach takes O(n), but we can make use of binary search as the array is already sorted
         char res = letters[0];
-        for (char letter : letters) {
-            if (letter > target) {
-                res = letter;
-                break;
+        int low = 0, high = letters.size() - 1;
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (letters[mid] > target) {
+                high = mid - 1;
+                res = letters[mid];
+            } else {
+                low = mid + 1;
             }
         }
         return res;
