2c060f2acf98eeb1f5d592a0e935cef3ce3b6b22
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-06-09 14:07:46 -0400

    Time: 21 ms (63.34%), Space: 19.5 MB (96.04%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -4,22 +4,50 @@ public:
         // majority element appears > n/2 times
         // can make use of bitwise operators
         
-        int n = nums.size();
-        int res = 0; // all bits are initially zero
+//         int n = nums.size();
+//         int res = 0; // all bits are initially zero
         
-        for (int i = 0; i < 32; i++) {
-            int bit = 1 << i;
-            int currBitCount = 0;
-            for (int num : nums) {
-                int currBit = num & bit;
-                if (currBit) { // if the currBit is set
-                    currBitCount++;
-                } 
-            }
-            if (currBitCount > n / 2) { // set the res to the currBit, so at the end after getting done with all the bits, we'll be making a majority element by ourselves.
-                res |= bit;
+//         for (int i = 0; i < 32; i++) {
+//             int bit = 1 << i;
+//             int currBitCount = 0;
+//             for (int num : nums) {
+//                 int currBit = num & bit;
+//                 if (currBit) { // if the currBit is set
+//                     currBitCount++;
+//                 } 
+//             }
+//             if (currBitCount > n / 2) { // set the res to the currBit, so at the end after getting done with all the bits, we'll be making a majority element by ourselves.
+//                 res |= bit;
+//             }
+//         }
+//         return res;
+        
+        // MOORE'S VOTING ALGO
+        int cnt = 0;
+        int res = nums[0];
+        
+        for (int num : nums) {
+            if (cnt == 0) {
+                res = num;
+                cnt = 1;
+            } else if (num == res) {
+                cnt++;
+            } else {
+                cnt--;
             }
         }
-        return res;
+        
+        cnt = 0;
+        
+        for (int num : nums) {
+            if (res == num) {
+                cnt++;
+            }
+        }
+        
+        if (cnt > nums.size() / 2) {
+            return res;
+        }
+        return 0;
     }
 };
