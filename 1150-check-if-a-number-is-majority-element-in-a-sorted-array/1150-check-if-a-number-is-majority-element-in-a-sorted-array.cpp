d86ff4e4c79014d696d85f293d5eaabb8d213e42
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-06-10 10:01:31 -0400

    Time: 3 ms (64.74%), Space: 7.7 MB (23.40%) - LeetHub

--- 1150-check-if-a-number-is-majority-element-in-a-sorted-array/1150-check-if-a-number-is-majority-element-in-a-sorted-array.cpp
+++ 1150-check-if-a-number-is-majority-element-in-a-sorted-array/1150-check-if-a-number-is-majority-element-in-a-sorted-array.cpp
@@ -1,10 +1,17 @@
 class Solution {
 public:
     bool isMajorityElement(vector<int>& nums, int target) {
-        int low = 0, high = nums.size() - 1;
-        int firstInstanceAt = 0;
-        int cnt = 0;
+        int n = nums.size();
+        int firstInstanceAt = findFirstInstance(nums, target);
         
+        // if the element at firstInstanceAt + nums.size() / 2 is still target, then it is a majority element. No need to check for the rest
+        return (firstInstanceAt + n / 2 < n) && (nums[firstInstanceAt + n / 2] == target);
+    }
+
+private:
+    int findFirstInstance(vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1;
+        int firstInstanceAt = 0;        
         // find the index of first instance 
         while (low <= high) {
             int mid = (low + high) / 2;
@@ -17,15 +24,23 @@ public:
                 high = mid - 1;
             }
         }
-        
-        for (int i = firstInstanceAt; i < nums.size(); i++) {
-            if (nums[i] != target) {
-                break;
+        return firstInstanceAt;
+    }
+    
+    int findLastInstance(vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1;
+        int lastInstanceAt = -1;        
+        // find the next index of last instance 
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] > target) {
+                lastInstanceAt = mid;
+                high = mid - 1;
+            } else if (target > nums[mid]) {
+                low = mid + 1;
             }
-            cnt++;
         }
-        
-        return cnt > nums.size() / 2;
+        return lastInstanceAt;
     }
     
 };
