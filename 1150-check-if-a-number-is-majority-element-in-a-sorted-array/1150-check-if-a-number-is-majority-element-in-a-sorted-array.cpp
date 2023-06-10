9375f4f179bc64a84baf6241bb8512384123de39
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-06-10 09:32:48 -0400

    Time: 3 ms (64.74%), Space: 7.6 MB (83.97%) - LeetHub

---
+++ 1150-check-if-a-number-is-majority-element-in-a-sorted-array/1150-check-if-a-number-is-majority-element-in-a-sorted-array.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isMajorityElement(vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1;
+        int firstInstanceAt = 0;
+        int cnt = 0;
+        
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if ((mid == 0 || nums[mid - 1] < target) && nums[mid] == target) {
+                firstInstanceAt = mid;
+                break;
+            } else if (target > nums[mid]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        
+        for (int i = firstInstanceAt; i < nums.size(); i++) {
+            if (nums[i] == target) {
+                cnt++;
+            }
+        }
+        
+        return cnt > nums.size() / 2;
+    }
+};
