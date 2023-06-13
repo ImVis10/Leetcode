2ba5d3542b500094129119c91e26b74ffeb17fd5
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-06-13 09:58:56 -0400

    Time: 1133 ms (5.02%), Space: 567.4 MB (5.02%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
@@ -1,25 +1,61 @@
 class Solution {
 public:
     int equalPairs(vector<vector<int>>& grid) {
-        int numRows = grid.size();
-        int numCols = numRows;
+//         int numRows = grid.size();
+//         int numCols = numRows;
         
+//         int cnt = 0;
+        
+//         for (int i = 0; i < numRows; i++) {
+//             for (int j = 0; j < numCols; j++) {
+//                 bool isEqual = true;
+                
+//                 for (int k = 0; k < numRows; k++) {
+//                     if (grid[i][k] != grid[k][j]) {
+//                         isEqual = false;
+//                         break;
+//                     }
+//                 }
+                
+//                 cnt += isEqual ? 1 : 0;
+//             }
+//         }
+//         return cnt;
+        
+        // BETTER APPROACH - USING HASHING O(n^2)
+        int n = grid.size();
         int cnt = 0;
         
-        for (int i = 0; i < numRows; i++) {
-            for (int j = 0; j < numCols; j++) {
-                bool isEqual = true;
-                
-                for (int k = 0; k < numRows; k++) {
-                    if (grid[i][k] != grid[k][j]) {
-                        isEqual = false;
-                        break;
-                    }
-                }
-                
-                cnt += isEqual ? 1 : 0;
+        map<string, int> map;
+        for (auto& row: grid) {
+            // CAN'T DO THIS.. GOTTA HAVE COMMAS AS [[11,1],[1,11]] ==> 111 AND 111. BUT BOTH THE ENTRIES ARE NOT THE SAME ONE IS [11, 1] AND OTHER IS [1, 11]
+            // ostringstream oss;
+            // copy(row.begin(), row.end(), ostream_iterator<int>(oss));
+            // string rowStr = oss.str();
+            string rowStr = join(row, ",");
+            map[rowStr] += 1;
+        }
+        
+        for (int i = 0; i < n; i++) {
+            vector<int> colArray(n);
+            for (int j = 0; j < n; j++) {
+                colArray[j] = grid[j][i];
             }
+            cnt += map[join(colArray, ",")];
         }
         return cnt;
+    }
+    
+private:
+    string join(vector<int> const &vec,string delim) {
+    if (vec.empty()) {
+        return string();
+    }
+ 
+    return accumulate(vec.begin() + 1, vec.end(), 
+    to_string(vec[0]),
+                [](const string& a, int b){
+                    return a + ", " + to_string(b);
+                });
     }
 };
