0b7f6caf5a04cf578074aecedce7fa5a345c0eb6
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-06-15 11:16:56 -0400

    Time: 241 ms (70.89%), Space: 123.8 MB (91.61%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -1,18 +1,35 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<int> positive;
+//         vector<int> negative;
+        
+//         for (int num : nums) {
+//             num > 0 ? positive.push_back(num) : negative.push_back(num);
+//         }
+                
+//         for (int i = 0; i < n / 2; i++) {
+//             nums[2*i] = positive[i];
+//             nums[2*i + 1] = negative[i];
+//         }
+//         return nums;
+        
+        // ITERATING ONLY ONCE
         int n = nums.size();
-        vector<int> positive;
-        vector<int> negative;
+        vector<int> res(n);
+        
+        int pos = 0, neg = 1;
         
         for (int num : nums) {
-            num > 0 ? positive.push_back(num) : negative.push_back(num);
+            if (num > 0) {
+                res[pos] = num;
+                pos += 2;
+            } else {
+                res[neg] = num;
+                neg += 2;
+            }
         }
-                
-        for (int i = 0; i < n / 2; i++) {
-            nums[2*i] = positive[i];
-            nums[2*i + 1] = negative[i];
-        }
-        return nums;
+        return res;
     }
 };
