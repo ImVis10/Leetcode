36e17f32b34f57203c2c9be51067f75a188fcc57
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-06-15 22:45:29 -0400

    Time: 28 ms (69.83%), Space: 22.3 MB (82.15%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        // the single element would be at an even index without its pair next to it
-        // elements after it are at even indexes not followed by their pair
+        // single element would be at even index
         int low = 0, high = nums.size() - 1;
-        while (low < high) {
+        if (nums.size() == 1) {
+            return nums[0];
+        }
+        while (low <= high) {
             int mid = low + (high - low) / 2;
-            mid = mid % 2 != 0 ? mid - 1 : mid;
+            mid = mid % 2 == 0 ? mid : mid - 1;
             if (nums[mid] == nums[mid + 1]) {
                 low = mid + 2;
             } else {
-                high = mid;
+                high = mid - 1;
             }
         }
         return nums[low];
