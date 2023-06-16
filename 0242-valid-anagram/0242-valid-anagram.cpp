92a3a6baa0ac72c5750603a2357e3e6fe1cb3eb2
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-06-16 11:34:10 -0400

    Time: 18 ms (21.64%), Space: 7.4 MB (43.30%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,24 +1,11 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        int len1 = s.length();
-        int len2 = t.length();
-        
-        if (len1 != len2) {
+        if (s.length() != t.length()) {
             return false;
         }
-        
-        unordered_map<char,int> map;
-        for (int i = 0; i < len1; i++) {
-            map[s[i]]++;
-            map[t[i]]--;
-        }
-        
-        for (auto x : map) {
-            if(x.second) {
-                return false;
-            }
-        }
-        return true;
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return s == t;
     }
 };
