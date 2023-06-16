a204e0addef33b5cee8a7ec1972ed4376c7a08a7
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-06-16 11:37:01 -0400

    Time: 11 ms (61.05%), Space: 7.4 MB (13.23%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -4,8 +4,22 @@ public:
         if (s.length() != t.length()) {
             return false;
         }
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        return s == t;
+        // sort(s.begin(), s.end());
+        // sort(t.begin(), t.end());
+        // return s == t;
+        
+        // ANSWER TO FOLLOW-UP QUESTION
+        map<char, int> map;
+        for (int i = 0; i < s.length(); i++) {
+            map[s[i]]++;
+            map[t[i]]--;
+        }
+        
+        for (auto entry : map) {
+            if (entry.second) {
+                return false;
+            }
+        }
+        return true;
     }
 };
