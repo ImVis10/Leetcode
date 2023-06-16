ff57d5db94192c15e44c7952eddb5468084b43af
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-06-16 11:30:31 -0400

    Time: 2312 ms (5.02%), Space: 69 MB (9.98%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,40 +1,26 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        // This approach takes o(n log n) which is not what is expected in the problem statement. So optimizing this to O(n), to increase time complexity space complexity needs to be increased most of the times as there is always a trade off between the two.
-        // if (nums.size() == 0) {
-        //     return 0;
-        // }
-        // sort(nums.begin(), nums.end());
-        // int maxSeq = 1, currSeq = 1;
-        // for (int i = 1; i < nums.size(); i++) {
-        //     if (nums[i] != nums[i - 1]) {
-        //         if (nums[i] == nums[i - 1] + 1) {
-        //             currSeq += 1;
-        //         } else {
-        //             maxSeq = max(currSeq, maxSeq);
-        //             currSeq = 1; // reset the length of the current sequnce formed
-        //         }
-        //     }
-        // }
-        // return max(currSeq, maxSeq);
-        if (nums.size() == 0) {
-            return 0;
-        }
-        unordered_set<int> set(nums.begin(), nums.end());
-        int maxSeq = 1, currSeq = 1;
+        set<int> set;
         for (int num : nums) {
-            if (!set.count(num - 1)) { // if num - 1 is not present in the set
-                int currNum = num;
-                currSeq = 1;
-            
-                while (set.count(currNum + 1)) {
-                    currNum += 1;
-                    currSeq += 1;
-                }
-                maxSeq = max(currSeq, maxSeq); 
-            }    
+            set.insert(num);
         }
-        return maxSeq;
+        
+        int res = 0;
+        
+        for (int num : nums) {
+            // check if 'num - 1' is present in the set
+            if (!set.count(num - 1)) {
+                int currNum = num;
+                int currSeqLength = 1;
+            
+                while (set.count(currNum + 1)) { // while the consecutive elements are present
+                    currNum += 1;
+                    currSeqLength += 1;
+                }
+                res = max(res, currSeqLength);
+            }
+        }
+        return res;
     }
 };
