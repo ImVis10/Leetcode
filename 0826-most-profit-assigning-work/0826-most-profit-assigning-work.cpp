041df54cc1a0ab27649e47f8a1c945de5c109b21
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-06-16 23:07:04 -0400

    Time: 177 ms (12.38%), Space: 286.6 MB (5.11%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.cpp
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.cpp
@@ -7,6 +7,11 @@ public:
         
         vector<int> arr(1000001, 0);
         
+        int maxDifficulty = *max_element(difficulty.begin(), difficulty.end());
+        int maxAbility = *max_element(worker.begin(), worker.end());
+        
+        int maxLevel = max(maxDifficulty, maxAbility);
+        
         // make a map of difficulty (key) --> profit (value)
         for (int i = 0; i < n; i++) {
             arr[difficulty[i]] = max(profit[i], arr[difficulty[i]]); // to take the max profit when there is duplicate difficulty
@@ -17,7 +22,7 @@ public:
         // Idea is a task with difficulty 'd' can be taken upon by a worker with ability >= d, and work on maximum profit tasks
         // eg: a task with difficulty '3' can be taken upon a worker with ability 4, 5, 6...
         // set the maximum profit attainable at each difficulty
-        for (int i = 0; i < arr.size(); i++) {
+        for (int i = 0; i <= maxLevel; i++) {
             maxProfit = max(maxProfit, arr[i]);
             arr[i] = maxProfit;
         }
