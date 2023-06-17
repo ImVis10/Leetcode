a98b5a346999aa286616ada0a3dca9c4dc5bbdf4
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-06-16 22:37:57 -0400

    Time: 552 ms (6.04%), Space: 286.6 MB (5.11%) - LeetHub

---
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
+        int n = profit.size();
+        int m = worker.size();
+        int res= 0;
+        
+        vector<int> arr(1000001, 0);
+        
+        // make a map of difficulty (key) --> profit (value)
+        for (int i = 0; i < n; i++) {
+            arr[difficulty[i]] = max(profit[i], arr[difficulty[i]]); // to take the max profit when there is duplicate difficulty
+        }
+        
+        int maxProfit = 0;
+        
+        // Idea is a task with difficulty 'd' can be taken upon by a worker with ability >= d, and work on maximum profit tasks
+        // eg: a task with difficulty '3' can be taken upon a worker with ability 4, 5, 6...
+        for (int i = 0; i < arr.size(); i++) {
+            maxProfit = max(maxProfit, arr[i]);
+            arr[i] = maxProfit;
+        }
+        
+        for (int i = 0; i < m; i++) {
+            res += arr[worker[i]];
+        }
+        return res;
+    }
+};
