e22e5707ee88f0c51943cfb198cf2ab86e005b81
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-06-17 21:40:37 -0400

    Time: 21 ms (13.19%), Space: 10.5 MB (9.34%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -11,8 +11,17 @@ public:
             return x.second < y.second;
         });
         
+        // Do bucket sort
+        // BUCKET SORT IS NOT A SORTING ALGO BASED ON COMPARISON.. SO IT TAKES O(n) not O(n log n)
+        // Number of buckets is equal to the maximum frequency + 1
+        // +1 because it is 0-indexing
         vector<vector<char>> buckets(maxVal->second + 1);
         
+        
+        // allocate chars for each bucket
+        // eg: TREE
+        // 0   1     2
+        //  {E, E}  {T}
         for (auto& entry : map) {
             int freq = map[entry.first];
             buckets[freq].push_back(entry.first);
