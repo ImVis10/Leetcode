ef8f2b677d5a5e0fc9904e6ed24df8291f9f10db
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-06-20 01:07:23 -0400

    Time: 244 ms (83.20%), Space: 130 MB (59.59%) - LeetHub

--- 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.cpp
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
     vector<int> getAverages(vector<int>& nums, int k) {
-        if (k == 0) {
+        if (k == 0) { // if window size is zero, average is every element divided by 1, so just return nums
             return nums;
         }
         
         int n = nums.size();
         vector<int> res(n , -1);
         
-        if (n < 2 * k + 1) {
+        int windowSize = 2 * k + 1;
+
+        if (n < windowSize) { // if window size greater than size of nums, diameter goes out of bounds for each element of the array
             return res;
         }
         
         long long sum = 0;
-        int windowSize = 2 * k + 1;
-        for (int i = 0; i < windowSize; i++) {
+        for (int i = 0; i < windowSize; i++) { // first window
             sum += nums[i];
         }
         
         res[k] = sum / windowSize;
         
         for (int i = windowSize; i < n; i++) {
-            sum = sum - nums[i - windowSize] + nums[i];
+            sum = sum - nums[i - windowSize] + nums[i]; // subtract the first element of previous window and add the new element in the current window
             res[i - k] = sum / windowSize;
         }
         return res;
