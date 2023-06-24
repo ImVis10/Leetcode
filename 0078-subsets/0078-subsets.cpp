753de70874e38ac28b083bf6d4eb2021f514b084
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-06-23 21:17:41 -0400

    Time: 9 ms (9.70%), Space: 15.9 MB (29.13%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> res;
+        vector<int> subRes;
+        
+        printAllSubSequences(0, n, nums, subRes, res);
+        return res;
+    }
+private:
+    void printAllSubSequences(int idx, int n, vector<int>& nums, vector<int> subRes, vector<vector<int>>& res) {
+        if (idx == n) {
+            res.push_back(subRes);
+            return;
+        }
+        subRes.push_back(nums[idx]);
+        printAllSubSequences(idx + 1, n, nums, subRes, res);
+        
+        subRes.pop_back();
+        printAllSubSequences(idx + 1, n, nums, subRes, res);
+    }
+};
