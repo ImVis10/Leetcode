18e17048fc5b948e79ba196036030c4c4fe7d1c8
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-06-23 20:32:13 -0400

    Time: 23 ms (68.13%), Space: 23.3 MB (24.02%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        rev(0, s.size() - 1, s);
+    }
+    
+private:
+    void rev(int l, int r, vector<char>& s) {
+        if (l >= r) {
+            return;
+        }
+        swap(s[l], s[r]);
+        rev(l + 1, r - 1, s);
+    }
+};
