d1b9f3c37be44fe90f6173f55d6fb1ca298beac0
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-06-25 14:32:48 -0400

    Time: 55 ms (23.59%), Space: 43.5 MB (19.22%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int> comb;
+        vector<vector<int>> res;
+        recurse(0, candidates, target, res, comb);
+        return res;
+    }
+    
+private:
+    void recurse(int idx, vector<int>& nums, int target, vector<vector<int>>& res, vector<int> comb) {
+        if (idx == nums.size()) {
+            if (target == 0) {
+                res.push_back(comb);
+            }
+            return;
+        }
+        
+        if (nums[idx] <= target) {
+            comb.push_back(nums[idx]);
+            recurse(idx, nums, target - nums[idx], res, comb);
+            comb.pop_back();
+        }
+        recurse(idx + 1, nums, target, res, comb);
+    }
+};
