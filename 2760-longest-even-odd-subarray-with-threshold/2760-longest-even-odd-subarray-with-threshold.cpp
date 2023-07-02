e09b76c4131add940979c9a9c386945e91b48138
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-07-02 12:04:35 -0400

    Time: 95 ms (87.50%), Space: 91.1 MB (25.00%) - LeetHub

--- 2760-longest-even-odd-subarray-with-threshold/2760-longest-even-odd-subarray-with-threshold.cpp
+++ 2760-longest-even-odd-subarray-with-threshold/2760-longest-even-odd-subarray-with-threshold.cpp
@@ -1,16 +1,34 @@
 class Solution {
 public:
     int longestAlternatingSubarray(vector<int>& a, int threshold) {
+//         int n = a.size();
+//         int res = 0;
+        
+//         for (int l = 0; l < n; l++) {
+//             if (a[l] % 2 == 1 || a[l] > threshold) continue;
+//             int r = l + 1;
+//             while (r < n && a[r] <= threshold && a[r] % 2 != a[r - 1] % 2) {
+//                 r++;
+//             }
+//             res = max(res, r - l);
+//         }
+//         return res;
+        
         int n = a.size();
-        int res = 0;
+        int cnt = 0, res = 0;
         
         for (int l = 0; l < n; l++) {
-            if (a[l] % 2 == 1 || a[l] > threshold) continue;
-            int r = l + 1;
-            while (r < n && a[r] <= threshold && a[r] % 2 != a[r - 1] % 2) {
-                r++;
+            if (a[l] <= threshold) {
+                if (cnt != 0) {
+                    cnt = a[l] % 2 != a[l - 1] % 2 ? cnt + 1 : 0;
+                }  
+                if (cnt == 0) {
+                    cnt = a[l] % 2 == 0;
+                }
+            } else {
+                cnt = 0;
             }
-            res = max(res, r - l);
+            res = max(res, cnt);
         }
         return res;
     }
