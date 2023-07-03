34829f69cfa5a945c726e873876ea26eb7216650
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-07-02 21:17:06 -0400

    Time: 3 ms (78.34%), Space: 6.9 MB (52.51%) - LeetHub

---
+++ 0859-buddy-strings/0859-buddy-strings.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool buddyStrings(string s, string goal) {
+//         if (goal.length() != s.length() || s.length() == 1) {
+//             return false;
+//         }
+        
+//         string temp = s;
+        
+//         for (int i = 0; i < s.length() - 1; i++) {
+//             for (int j = i + 1; j < s.length(); j++) {
+//                 swap(s[i], s[j]);
+//                 if (s == goal) {
+//                     return true;
+//                 }
+//                 s = temp;
+//             }
+//         }
+//         return false;
+        
+        if (goal.length() != s.length() || s.length() == 1) {
+            return false;
+        }
+        
+        if (s == goal) {
+            set<char> set(s.begin(), s.end());
+            return set.size() < goal.size();
+        }
+        
+        int l = 0, r = s.length() - 1;
+        
+        while (l < r && s[l] == goal[l]) {
+            l++;
+        }
+        
+        while (r >= 0 && s[r] == goal[r]) {
+            r--;
+        }
+        
+        swap(s[l], s[r]);
+        
+        return s == goal;
+    }
+};
