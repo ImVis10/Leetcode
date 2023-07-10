2bde2b343ebb5fd829f849035adb5e85f1b1bad9
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-07-10 08:05:53 -0400

    Time: 3 ms (74.03%), Space: 7.5 MB (6.66%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        int i = 0, n = s.length();
-        int sign = 1;
+        int n = s.length();
+        int i = 0;
         
         while (i < n && s[i] == ' ') {
             i++;
         }
+        
+        int sign = 1;
         
         if (s[i] == '-') {
             sign = -1;
@@ -14,19 +16,20 @@ public:
         } else if (s[i] == '+') {
             i++;
         }
-        return atoi(s, sign , i, 0);
+        
+        return recurse(i, s, sign, 0);
     }
-    
+        
 private:
-    long atoi(string s, int sign, int idx, long res) {
-        if (sign * res >= INT_MAX) return INT_MAX;
-        if (sign * res <= INT_MIN) return INT_MIN;
+    long recurse(int idx, string s, int sign, long res) {
+        if (sign * res > INT_MAX) return INT_MAX;
+        if (sign * res < INT_MIN) return INT_MIN;
         
         if (idx >= s.length() || s[idx] < '0' || s[idx] > '9') {
             return sign * res;
         }
         
-        res = atoi(s, sign, idx + 1, (res * 10 + (s[idx] - '0')));
+        res = recurse(idx + 1, s, sign, res * 10 + s[idx] - '0');
         return res;
     }
 };
