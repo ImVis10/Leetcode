6c40ff3bb37ec6f3fc7904bd8a4bdedbc9188ca2
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-07-14 14:42:44 -0400

    Time: 241 ms (60.98%), Space: 76.2 MB (64.20%) - LeetHub

--- 2453-destroy-sequential-targets/2453-destroy-sequential-targets.cpp
+++ 2453-destroy-sequential-targets/2453-destroy-sequential-targets.cpp
@@ -5,21 +5,33 @@ public:
         // numbers having same remainder when divided by space can be expressed in 'num + c * space'
         // eg: [1, 3, 99], space = 2. All have remainder = 1. So 1 + 0(2) = 1, 1 + 1(2) = 3, 1 + 49(2) = 99
         // so let's keep track of remainders of each element
-        unordered_map<int, vector<int>> map;
-        for (auto& num : nums) {
-            map[num % space].push_back(num);
-        }
+//         unordered_map<int, vector<int>> map;
+//         for (auto& num : nums) {
+//             map[num % space].push_back(num);
+//         }
         
+//         int maxCount = 0;
+//         // now it's just about getting the least value in max sized values
+//         for (auto& pair : map) {
+//             if (pair.second.size() > maxCount) maxCount = pair.second.size();
+//         }
+//         int res = INT_MAX;
+//         for (auto& pair : map) {
+//             if (pair.second.size() == maxCount) {
+//                 for (auto& num : pair.second) res = min(res, num);
+//             }
+//         }
+//         return res;
+        // A BIT MORE EFFICIENT APPROACH. LOOPS THROUGH JUST NUMS TWICE. NO NEED TO LOOP THROUGH MAP. JUST USE MAP TO FIND VALUE BASED ON KEY
+        unordered_map<int, int> map;
         int maxCount = 0;
-        // now it's just about getting the least value in max sized values
-        for (auto& pair : map) {
-            if (pair.second.size() > maxCount) maxCount = pair.second.size();
+        for (auto& num : nums) {
+            map[num % space]++;
+            maxCount = max(maxCount, map[num % space]);   
         }
         int res = INT_MAX;
-        for (auto& pair : map) {
-            if (pair.second.size() == maxCount) {
-                for (auto& num : pair.second) res = min(res, num);
-            }
+        for (auto& num : nums) {
+            if (map[num % space] == maxCount) res = min(res, num);
         }
         return res;
     }
