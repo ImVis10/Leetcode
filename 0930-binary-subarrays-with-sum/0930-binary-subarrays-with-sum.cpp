f2a1a373bd9cc0e2a2dce82b4c80863905c610a3
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-07-15 11:52:57 -0400

    Time: 28 ms (95.49%), Space: 28.7 MB (94.41%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -1,13 +1,32 @@
 class Solution {
 public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-        int prefixSum = 0;
-        unordered_map<int, int> map({{0, 1}});
+        // int prefixSum = 0;
+        // unordered_map<int, int> map({{0, 1}});
+        // int res = 0;
+        // for (auto& num : nums) {
+        //     prefixSum += num;
+        //     res += map[prefixSum - goal];
+        //     map[prefixSum]++;
+        // }
+        // return res;
+        
+        // USING SLIDING WINDOW
+        return helper(nums, goal) - helper(nums, goal - 1);
+        
+    }
+private: 
+    int helper(vector<int>& nums, int goal) {
+        if (goal < 0) return 0;
         int res = 0;
-        for (auto& num : nums) {
-            prefixSum += num;
-            res += map[prefixSum - goal];
-            map[prefixSum]++;
+        int n = nums.size(), sum = 0;
+        
+        for (int i = 0, j = 0; j < n; j++) {
+            sum += nums[j];
+            while (sum > goal) {
+                sum -= nums[i++];
+            }
+            res += j - i + 1;
         }
         return res;
     }
