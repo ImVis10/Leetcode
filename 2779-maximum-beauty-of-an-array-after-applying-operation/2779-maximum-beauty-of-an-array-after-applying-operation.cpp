e449fe725bd9204bbb76c90c696ce0a596a94b33
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-07-16 16:41:51 -0400

    Time: 932 ms (11.11%), Space: 187.2 MB (11.11%) - LeetHub

--- 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.cpp
+++ 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.cpp
@@ -1,14 +1,27 @@
 class Solution {
 public:
     int maximumBeauty(vector<int>& nums, int k) {
-        int n = nums.size();
-        int res = 1;
-        sort(nums.begin(), nums.end());
-        int maxValInRange = 0;
-        for (int start = 0, end = 1; start < n; start++) {
-            maxValInRange = nums[start] + k;
-            while (end < n && maxValInRange >= nums[end++] - k) res++;
-            res = max(res, start - end + 1);
+        // int n = nums.size();
+        // int res = 1;
+        // sort(nums.begin(), nums.end());
+        // int maxValInRange = 0;
+        // for (int start = 0, end = 1; start < n; start++) {
+        //     maxValInRange = nums[start] + k;
+        //     while (end < n && maxValInRange >= nums[end++] - k) res++;
+        //     res = max(res, start - end + 1);
+        // }
+        // return res;
+        
+        // VAMSI'S APPROACH
+        map<int, int> map;
+        for (auto& num : nums) {
+            map[num - k] += 1;
+            map[num + k + 1] -= 1;
+        }
+        int sum = 0, res = 0;
+        for (auto&[key, val] : map) {
+            sum += val;
+            res = max(res, sum);
         }
         return res;
     }
