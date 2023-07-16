a4df20063de26f0c78fff2c29690feb2d4684ab3
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-07-16 08:25:04 -0400

    Time: 201 ms (54.60%), Space: 71.2 MB (56.73%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -1,38 +1,17 @@
 class Solution {
 public:
-    int totalFruit(vector<int>& fruits) {
-        // seems like a sliding window problem
-        // so we have to find the widest sliding window consisting of two unique elements
-        map<int, int> map;
-        int baskets = 2; // window size
+    int totalFruit(vector<int>& f, int k = 2) {
+        int n = f.size();
         int res = 0;
-        int prev = 0;
-        for (int i = 0;i < fruits.size(); i++) {
-            map[fruits[i]] += 1;
-            
-            while(map.size() > baskets) {
-                map[fruits[prev]] -= 1;
-                if (map[fruits[prev]] == 0) {
-                    map.erase(fruits[prev]);
-                }
-                prev++;
+        unordered_map<int, int> freq;
+        for (int i = 0, j = 0; j < n; j++) {
+            freq[f[j]]++;
+            if (freq.size() > k) {
+                if (--freq[f[i]] == 0) freq.erase(f[i]);
+                i++;
             }
-            
-            res = max(res, i - prev + 1);
+            res = max(res, j - i + 1);
         }
-        
-//         for(auto x : map) {
-//             res += x.second;
-//         }
-        
-//         int window_sum = res;
-        
-//         for (int i = baskets; i < fruits.size(); i++) {
-//             // if the type of fruit already exists in the basket, add to the count of trees
-//             // 
-//             window_sum += map[i] - map[i - baskets];
-//             res = max(res, window_sum);
-//         }
         return res;
     }
 };
