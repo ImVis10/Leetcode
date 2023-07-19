58db0bf59c4cc9dd80733820f970ee2fbdb72271
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-07-18 23:08:36 -0400

    Time: 21 ms (30.14%), Space: 8.2 MB (12.04%) - LeetHub

---
+++ 0340-longest-substring-with-at-most-k-distinct-characters/0340-longest-substring-with-at-most-k-distinct-characters.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int lengthOfLongestSubstringKDistinct(string s, int k) {
+        int n = s.length();
+        int res = 0;
+        unordered_map<char, int> freq;
+        for (int i = 0, j = 0; j < n; j++) {
+            freq[s[j]]++;
+            while (freq.size() > k) {
+                if (--freq[s[i]] == 0)  {
+                    freq.erase(s[i]);
+                    i++;
+                } else {
+                    i++;
+                }
+            }
+            res = max(res, j - i + 1);
+        }
+        return res;
+    }
+};
