d209388958d75c52db17e8f56332612526b737ea
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-07-19 08:47:35 -0400

    Time: 464 ms (80.82%), Space: 89.6 MB (94.24%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -17,21 +17,41 @@ public:
 //         }
 //         return n - dp[n-1];
         
-        // DP must be an overkill for this problem I guess;
+        // Getting TLE. DP must be an overkill for this problem I guess;
+        
+//         int n = intervals.size();
+//         sort(intervals.begin(), intervals.end());
+        
+//         int res = 0;
+        
+//         for (int i = 1; i < n; i++) {
+//             if (intervals[i][0] >= intervals[i - 1][1]) continue; // if no overlap, go to next one (if there is an interval present)
+//             // if there is a overlap
+//             intervals[i][0] = intervals[i - 1][0];
+//             intervals[i][1] = min(intervals[i][1], intervals[i - 1][1]);
+//             res++;
+//         }
+//         return res;
+        
+        // BUT THE CATCH HERE IS I'M MODIFYING THE INPUT, WHICH IS NOT EXACTLY ADVISABLE IN THE INTERVIEWS AND GENERALLY NOT CONSIDERED A GOOD PRACTICE
+        
+        // NOW I'M LOOKING AT THE EDITORIAL
+        // SHIT.. I'M DUMB
         
         int n = intervals.size();
-        sort(intervals.begin(), intervals.end());
+        sort(intervals.begin(), intervals.end(), [](const vector<int>& v1, const vector<int>& v2) {
+            return v1[1] < v2[1];
+        });
         
-        int res = 0;
+        int maxEndTimeSoFar = INT_MIN, res = 0;
         
-        for (int i = 1; i < n; i++) {
-            if (intervals[i][0] >= intervals[i - 1][1]) continue; // if no overlap, go to next one (if there is an interval present)
-            // if there is a overlap
-            intervals[i][0] = intervals[i - 1][0];
-            intervals[i][1] = min(intervals[i][1], intervals[i - 1][1]);
-            res++;
+        for (int i = 0; i < n; i++) {
+            int start = intervals[i][0];
+            int end = intervals[i][1];
+            
+            if (start >= maxEndTimeSoFar) maxEndTimeSoFar = end; // update the max endtime until that interval
+            else res++; // # overlapping intervals
         }
         return res;
-        // BUT THE CATCH HERE IS I'M MODIFYING THE INPUT, WHICH IS NOT EXACTLY ADVISABLE IN THE INTERVIEWS AND GENERALLY NOT CONSIDERED A GOOD PRACTICE
     }
 };
