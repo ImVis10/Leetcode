a9da978b37111dfcd82b88985f21a7c2a55be9e4
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-07-19 08:35:25 -0400

    Time: 543 ms (23.95%), Space: 89.7 MB (58.83%) - LeetHub

---
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+//         if(intervals.size() <= 1) return 0;
+
+//         sort(intervals.begin(), intervals.end());
+
+//         int n= intervals.size();
+//         vector<int> dp(n,1);
+
+//         for(int i = 1; i < n; i++){
+//             for(int j = i - 1; j >= 0; j--){
+//                 if(intervals[j][1] <= intervals[i][0]){ // non-overlapping
+//                     dp[i] = max(dp[i], dp[j] + 1);
+//                 }
+//             }
+//         }
+//         return n - dp[n-1];
+        
+        // DP must be an overkill for this problem I guess;
+        
+        int n = intervals.size();
+        sort(intervals.begin(), intervals.end());
+        
+        int res = 0;
+        
+        for (int i = 1; i < n; i++) {
+            if (intervals[i][0] >= intervals[i - 1][1]) continue; // if no overlap, go to next one (if there is an interval present)
+            // if there is a overlap
+            intervals[i][0] = intervals[i - 1][0];
+            intervals[i][1] = min(intervals[i][1], intervals[i - 1][1]);
+            res++;
+        }
+        return res;
+    }
+};
