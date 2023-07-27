63ae6156a175150feaf80bd46a8e46f914870871
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-07-26 21:25:17 -0400

    Time: 354 ms (75.16%), Space: 94.5 MB (49.76%) - LeetHub

--- 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.cpp
+++ 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.cpp
@@ -6,13 +6,13 @@ public:
         
         while (low <= high) {
             long long mid = low + (high - low) / 2;
-            if (isTimeTakenMinimum(time, mid, totalTrips)) high = mid - 1;
+            if (canMakeTotalTrips(time, mid, totalTrips)) high = mid - 1;
             else low = mid + 1;
         }
         return low;
     }
 private:
-    bool isTimeTakenMinimum(vector<int>& time, long long mid, int totalTrips) {
+    bool canMakeTotalTrips(vector<int>& time, long long mid, int totalTrips) {
         int tripsMade = 0;
         for (int i = 0; i < time.size(); i++) {
             if (tripsMade < totalTrips) tripsMade += mid / time[i];
