549015dbdb940fb38c59d135bfc37f259beaeb8b
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-07-31 12:34:28 -0400

    Time: 43 ms (77.99%), Space: 31.4 MB (94.84%) - LeetHub

--- 1060-missing-element-in-sorted-array/1060-missing-element-in-sorted-array.cpp
+++ 1060-missing-element-in-sorted-array/1060-missing-element-in-sorted-array.cpp
@@ -1,15 +1,38 @@
 class Solution {
 public:
     int missingElement(vector<int>& nums, int k) {
+        // int n = nums.size();
+        // int low = nums[0];
+        // for (int i = 1; i < n; i++) {
+        //     while (nums[i] != ++low) {
+        //         if (--k == 0) {
+        //             return low;
+        //         }
+        //     }
+        // }
+        // return nums[n - 1] + k;
+        
+        // USING BINARY SEARCH
+        
         int n = nums.size();
-        int low = nums[0];
-        for (int i = 1; i < n; i++) {
-            while (nums[i] != ++low) {
-                if (--k == 0) {
-                    return low;
-                }
+        int low = 0, high = n;        
+        while (low < high - 1) {
+            int mid = low + (high - low) / 2;
+            int numMissing = numberOfMissingElementsUntil(mid, nums);
+            if (numMissing < k) {
+                low = mid;
+            } else {
+                high = mid;
             }
         }
-        return nums[n - 1] + k;
+        return nums[low] + k - numberOfMissingElementsUntil(low, nums);
+    }
+private:
+    int numberOfMissingElementsUntil(int idx, vector<int>& nums) {
+        if (idx == 0) {
+            return 0;
+        }
+        int res = nums[idx] - nums[0] - idx;
+        return res;
     }
 };
