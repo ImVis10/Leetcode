aa5a371c094cdab5d6bf59de762e4369930a2e42
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-08-04 16:48:56 -0400

    Time: 4 ms (42.78%), Space: 7.6 MB (80.70%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,3 +1,32 @@
+// class Solution {
+// public:
+//     vector<vector<int>> permute(vector<int>& nums) {
+//         vector<int> perm;
+//         vector<vector<int>> res;
+//         vector<bool> picked(nums.size(), false);
+//         recurse(nums, perm, picked, res);
+//         return res;
+//     }
+// private:
+//     void recurse(vector<int> nums, vector<int> perm, vector<bool>& picked, vector<vector<int>>& res) {
+//         if (perm.size() == nums.size()) {
+//             res.push_back(perm);
+//             return;
+//         }
+        
+//         for (int i = 0; i < nums.size(); i++) {
+//             if (!picked[i]) {
+//                 picked[i] = true;
+//                 perm.push_back(nums[i]);
+//                 recurse(nums, perm, picked, res);
+            
+//                 perm.pop_back(); // backtrack 
+//                 picked[i] = false;
+//             }
+//         }
+//     }
+// };
+
 class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
@@ -7,16 +36,16 @@ public:
     }
     
 private:
-    void recurse(int idx, vector<int>& arr, vector<vector<int>>& res) {
-      if (idx == arr.size()) {
-        res.push_back(arr);
+    void recurse(int idx, vector<int>& nums, vector<vector<int>>& res) {
+      if (idx == nums.size()) {
+        res.push_back(nums);
         return;
       }
 
-      for (int i = idx; i < arr.size(); i++) {
-        swap(arr[i], arr[idx]);
-        recurse(idx + 1, arr, res);
-        swap(arr[i], arr[idx]); // to take the array back to its original
+      for (int i = idx; i < nums.size(); i++) {
+        swap(nums[i], nums[idx]);
+        recurse(idx + 1, nums, res);
+        swap(nums[i], nums[idx]); // backtrack
       }
     }
 };
