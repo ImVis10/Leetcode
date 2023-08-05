e89950733d8e6df108574a6587ee499bc429aa0a
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-08-05 16:07:35 -0400

    Time: 21 ms (43.89%), Space: 16.8 MB (7.20%) - LeetHub

--- 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.cpp
+++ 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.cpp
@@ -12,17 +12,23 @@
 class Solution {
 public:
     vector<TreeNode*> generateTrees(int n) {
+        // unordered_map<pair<int, int>, vector<TreeNode*>> cache;
         return actuallyGenerateTrees(1, n);
     }
 private:
     vector<TreeNode*> actuallyGenerateTrees(int left, int right) {
-        if (left == right) { // if tree has only 1 node
-            return vector<TreeNode*>{new TreeNode(left)};
-        }
+        // if (left == right) { // if tree has only 1 node
+        //     return vector<TreeNode*>{new TreeNode(left)};
+        // } 
+        // NOT NEEDED AS IT IS ALREADY BEING COMPUTED BY THE FOR LOOPS BELOW
         
         if (left > right) {
             return vector<TreeNode*>{nullptr};
         }
+        
+        // if (map.find({left, right}) != map.end()) {
+        //     return cache[{left, right}];
+        // }
         
         vector<TreeNode*> res;
         for (int node = left; node <= right; node++) { // rooted at node
@@ -33,6 +39,7 @@ private:
                 }
             }
         }
+        // cache[{left, right}] = res;
         return res;
     }
 };
