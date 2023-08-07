bd2ef0e150d34215a05b1f2be5414a449797af85
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-08-07 15:43:40 -0400

    Time: 3 ms (82.65%), Space: 9.5 MB (19.85%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -4,37 +4,53 @@ public:
         // each row is sorted in non decreasing order
         // so can do binary search, to find out the row
         
-        int numRows = matrix.size(), numCols = matrix[0].size();
-        int topRow = 0, bottomRow = numRows - 1;
-        int currRow = 0;
+//         int numRows = matrix.size(), numCols = matrix[0].size();
+//         int topRow = 0, bottomRow = numRows - 1;
+//         int currRow = 0;
         
-        // finding out the row
-        while (topRow <= bottomRow) {
-            currRow = topRow + (bottomRow - topRow) / 2;
-            if (target > matrix[currRow][numCols - 1]) {
-                topRow = currRow + 1;
-            } else if (target < matrix[currRow][0]) {
-                bottomRow = currRow - 1;
-            } else { // this might be the row
-                break;
-            }
-        }
+//         // finding out the row
+//         while (topRow <= bottomRow) {
+//             currRow = topRow + (bottomRow - topRow) / 2;
+//             if (target > matrix[currRow][numCols - 1]) {
+//                 topRow = currRow + 1;
+//             } else if (target < matrix[currRow][0]) {
+//                 bottomRow = currRow - 1;
+//             } else { // this might be the row
+//                 break;
+//             }
+//         }
         
         
-        // check if the element is present in the row
-        int low = 0, high = numCols - 1;
-        int targetRow = currRow;
+//         // check if the element is present in the row
+//         int low = 0, high = numCols - 1;
+//         int targetRow = currRow;
+        
+//         while (low <= high) {
+//             int mid = low + (high - low) / 2;
+//             if (target == matrix[targetRow][mid]) {
+//                 return true;
+//             } else if (target > matrix[targetRow][mid]) {
+//                 low = mid + 1;
+//             } else {
+//                 high = mid - 1;
+//             }
+//         }
+//         return false;
+        int rows = matrix.size(), cols = matrix[0].size();
+        int low = 0, high = (rows * cols) - 1;
         
         while (low <= high) {
             int mid = low + (high - low) / 2;
-            if (target == matrix[targetRow][mid]) {
-                return true;
-            } else if (target > matrix[targetRow][mid]) {
+            int row = (mid / cols);
+            int col = (mid % cols);
+            if (target == matrix[row][col]) {
+                return 1;
+            } else if (target > matrix[row][col]) {
                 low = mid + 1;
             } else {
                 high = mid - 1;
             }
         }
-        return false;
+        return 0;
     }
 };
