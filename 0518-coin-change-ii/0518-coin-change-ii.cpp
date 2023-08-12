4d2f87fa3b6c88e8b7e3760b03ebae80d6bf70b9
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-08-12 11:57:55 -0400

    Time: 41 ms (40.46%), Space: 18.3 MB (65.73%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -19,25 +19,47 @@ public:
         
         // GETTING TLE AS THE TIME COMPLEXITY IS O (target) and lot of overlapping sub problems which should not be computed multiple no. of times
         // so gotta memoize this
-        vector<vector<int>> dp(coins.size(), vector<int>(amount + 1, -1));
-        return recurse(coins.size() - 1, amount, coins, dp);
-    }
-private:
-    int recurse(int idx, int target, vector<int>& coins, vector<vector<int>>& dp) {
-        if (idx == 0) {
-            return (target % coins[idx] == 0);
+        // vector<vector<int>> dp(coins.size(), vector<int>(amount + 1, -1));
+        // return recurse(coins.size() - 1, amount, coins, dp);
+    // }
+// private:
+//     int recurse(int idx, int target, vector<int>& coins, vector<vector<int>>& dp) {
+//         if (idx == 0) {
+//             return (target % coins[idx] == 0);
+//         }
+        
+//         if (dp[idx][target] != -1) {
+//             return dp[idx][target];
+//         }
+        
+//         int noPick = recurse(idx - 1, target, coins, dp);
+//         int pick = 0;
+//         if (coins[idx] <= target) {
+//             pick = recurse(idx, target - coins[idx], coins, dp);
+//         }
+//         return dp[idx][target] = pick + noPick;
+//     }
+        
+        // CONVERTING TO TABULATION
+        int n =  coins.size();
+        vector<vector<int>> dp(n, vector<int>(amount + 1, 0));
+        
+        for (int t = 0; t <= amount; t++) { // Base case
+            dp[0][t] = (t % coins[0] == 0);
         }
         
-        if (dp[idx][target] != -1) {
-            return dp[idx][target];
+        for (int idx = 1; idx < n; idx++) {
+            for (int t = 0; t <= amount; t++) {
+                int noPick = dp[idx - 1][t];
+                int pick = 0;
+                if (coins[idx] <= t) {
+                    pick = dp[idx][t - coins[idx]];
+                }
+                dp[idx][t] = pick + noPick;
+            }
         }
+        return dp[n - 1][amount];
         
-        int noPick = recurse(idx - 1, target, coins, dp);
-        int pick = 0;
-        if (coins[idx] <= target) {
-            pick = recurse(idx, target - coins[idx], coins, dp);
-        }
-        return dp[idx][target] = pick + noPick;
     }
     
 };
