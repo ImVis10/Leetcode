99e5f6382a5092854f5224ed9577786c5f9b9b9d
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-08-21 05:58:37 -0400

    Time: 55 ms (27.77%), Space: 91.9 MB (19.07%) - LeetHub

---
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        int len = s.length();
+        string substr = "";
+        for (int i = 0; i < len; i++) {
+            substr += s[i];
+            int lenSubStr = substr.length();
+            if (len % lenSubStr != 0) continue;
+            string repeatedSubStr = "";
+            for (int j = 0; j < (len / lenSubStr); j++) {
+                repeatedSubStr += substr;
+            }
+            if (s == repeatedSubStr && len != lenSubStr) return true;
+        }
+        
+        return false;
+    }
+};
