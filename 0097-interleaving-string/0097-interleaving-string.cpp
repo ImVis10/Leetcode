03803d2e44daac8ab22c44bc243099eb52ee71cf
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-08-25 07:53:20 -0400

    Time: 15 ms (40.52%), Space: 25.6 MB (30.48%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -7,24 +7,15 @@ public:
     }
 private:
     bool recurse(int idx1, string s1, int idx2, string s2, string s3, vector<vector<int>>& cache) {
-        if (s1.length() + s2.length() != s3.length()) {
-            return false;
-        }
-        if (idx1 == s1.length() and idx2 == s2.length()) {
-            return true;
-        }
+        if (s1.length() + s2.length() != s3.length()) return false;
         
-        if (cache[idx1][idx2] != -1) {
-            return cache[idx1][idx2];
-        }
+        if (idx1 == s1.length() and idx2 == s2.length()) return true;
         
-        if (idx1 < s1.length() and s1[idx1] == s3[idx1 + idx2] and recurse(idx1 + 1, s1, idx2, s2, s3, cache)) {
-            return true;
-        }
+        if (cache[idx1][idx2] != -1) return cache[idx1][idx2];
         
-        if (idx2 < s2.length() and s2[idx2] == s3[idx1 + idx2] and recurse(idx1, s1, idx2 + 1, s2, s3, cache)) {
-            return true;
-        }
+        if (idx1 < s1.length() and s1[idx1] == s3[idx1 + idx2] and recurse(idx1 + 1, s1, idx2, s2, s3, cache)) return true;
+        
+        if (idx2 < s2.length() and s2[idx2] == s3[idx1 + idx2] and recurse(idx1, s1, idx2 + 1, s2, s3, cache)) return true;
         
         return cache[idx1][idx2] = 0;
     }
