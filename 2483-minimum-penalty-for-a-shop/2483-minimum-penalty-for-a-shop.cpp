db31c3ef559d95ccb6ce5da26e84f0693c5dbf22
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-08-29 13:05:05 -0400

    Time: 46 ms (22.52%), Space: 18.9 MB (15.96%) - LeetHub

---
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    int bestClosingTime(string customers) {
+        // int close = 0;
+        // int res = INT_MAX;
+        // int pos = -1;
+        // while (close <= customers.length()) {
+        //     int pen = 0;
+        //     for (int i = 0; i < customers.length(); i++) {
+        //         if (close > i) {
+        //             if (customers[i] == 'N') {
+        //                 pen++;
+        //             }
+        //         } else {
+        //             if (customers[i] == 'Y') {
+        //                 pen++;
+        //             }
+        //         }
+        //     }
+        //     if (pen < res) {
+        //         res = pen;
+        //         pos = close;
+        //     } 
+        //     close++; 
+        // }
+        // return pos;
+        
+        // GETTING TLE
+        
+        // so should store the computed results
+        
+        int n = customers.length();
+        vector<int> prefixN(n + 1, 0);
+        vector<int> suffixY(n + 1, 0);
+        
+        for (int i = 1; i <= n; i++) { // close from 1
+            prefixN[i] = prefixN[i - 1];
+            if (customers[i - 1] == 'N') { // shop is open, but no customers visit
+                prefixN[i] += 1;
+            }
+        }
+        
+        for (int i = n - 1; i >= 0; i--) { // close from n
+            suffixY[i] = suffixY[i + 1];
+            if (customers[i] == 'Y') { // shop is closed, but customers visit
+                suffixY[i] += 1;
+            }
+        }
+        
+        int minPen = INT_MAX, res = -1;
+        
+        for (int i = 0; i <= n; i++) {
+            int pen = prefixN[i] + suffixY[i];
+            if (pen < minPen) {
+                minPen = pen;
+                res = i;
+            }
+        }
+        return res;
+    }
+};
