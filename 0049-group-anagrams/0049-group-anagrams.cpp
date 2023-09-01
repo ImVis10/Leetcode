6b26a0b69542b8954b843b8827f19e326276f51a
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-09-01 17:19:46 -0400

    Time: 20 ms (97.85%), Space: 19.7 MB (79.72%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        // check if each element is an anagram of the other and group them accordingly.
-        // is this efficient? Nah.. checking if two strings are anagrams would take O(n), so lets say for K strings and group them together would take around O(nk) I guess
-        vector<vector<string>> res;
         unordered_map<string, vector<string>> map;
-        for (int i = 0; i < strs.size(); i++) {
-            string str = strs[i];
-            sort(strs[i].begin(), strs[i].end()); // sort the string in the increasing order of its characters
-            map[strs[i]].push_back(str); // key(sorted_String) --> value(initial _string);
+        
+        for (auto& str : strs) {
+            string s = str;
+            sort(str.begin(), str.end());
+            map[str].push_back(s);
         }
         
+        vector<vector<string>> res;
         for (auto i = map.begin(); i != map.end(); i++) {
-            res.push_back(i -> second); // adding the list of anagrams to result
+            res.push_back(i->second);
         }
         return res;
     }
