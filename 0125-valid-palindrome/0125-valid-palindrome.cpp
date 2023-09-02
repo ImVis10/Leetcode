dabb2cb745538a6d416461bb20057caf713dc6c2
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-09-02 12:24:53 -0400

    Time: 9 ms (20.53%), Space: 7.7 MB (19.78%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        for (int left = 0, right = s.length() - 1; left < right; left++, right--) {
-            while (left < right && !isalnum(s[left])) {
-                left++;
+        string noSpecialCharString = "";
+        
+        for (char c : s) {
+            if (c >= 'A' && c <= 'Z') {
+                noSpecialCharString += tolower(c);                
+            } else if (c >= 'a' && c <= 'z' || c >= '0' && c <= '9') {
+                noSpecialCharString += c;
             }
-            while (left < right && !isalnum(s[right])) {
-                right--;
-            }
-            if (tolower(s[left]) != tolower(s[right])) {
-                return false;
-            }
+        }
+        return checkPalindrome(noSpecialCharString);
+    }
+private:
+    bool checkPalindrome(string noSpecialCharString) {
+        int n = noSpecialCharString.size();
+        for (int i = 0; i < n / 2; i++) {
+            if (noSpecialCharString[i] != noSpecialCharString[n - i - 1]) return false;
         }
         return true;
     }
