eca4cd86da74cda7e66c9eab10e9bf1638cdf390
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-09-08 09:27:10 -0400

    Time: 1544 ms (35.24%), Space: 758.6 MB (5.00%) - LeetHub

---
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    int minCut(string s) {        
+        vector<int> cache(s.length(), -1);
+        
+        // precompute palindrome check
+        vector<vector<bool>> isPalindrome(s.length(), vector<bool>(s.length(), false));
+        precomputePalindromeCheck(s, isPalindrome);
+        
+        int minCuts = recurse(0, s, cache, isPalindrome);
+        return minCuts - 1;
+    }
+private:
+    int recurse(int idx, string s, vector<int>& cache, const vector<vector<bool>>& isPalindrome) {
+        if (idx == s.length()) {
+            return 0;
+        }
+        
+        if (cache[idx] != -1) {
+            return cache[idx];
+        }
+        
+        int minCuts = INT_MAX;
+        
+        for (int i = idx; i < s.length(); i++) {
+            if (isPalindrome[idx][i]) {
+                int cuts = 1 + recurse(i + 1, s, cache, isPalindrome);
+                minCuts = min(cuts, minCuts);
+            }
+        }
+        
+        return cache[idx] = minCuts;
+    }
+    
+    void precomputePalindromeCheck(string s, vector<vector<bool>> &isPalindrome) {
+        int n = s.length();
+
+        // Every character is a palindrome by itself
+        for (int i = 0; i < n; i++) {
+            isPalindrome[i][i] = true;
+        }
+
+        // Check for two-character palindromes
+        for (int i = 0; i < n - 1; i++) {
+            isPalindrome[i][i + 1] = (s[i] == s[i + 1]);
+        }
+
+        // Check for longer palindromes
+        for (int len = 3; len <= n; len++) {
+            for (int i = 0; i <= n - len; i++) {
+                int j = i + len - 1;
+                isPalindrome[i][j] = (s[i] == s[j] && isPalindrome[i + 1][j - 1]);
+            }
+        }
+    }
+};
