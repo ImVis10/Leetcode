5dc6c81f86de92b97bf716882d667128a73cd32f
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-09-08 17:39:18 -0400

    Time: 75 ms (47.01%), Space: 59.1 MB (32.58%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-    int maxArea(vector<int>& height) {
+    int maxArea(vector<int>& height) { // two pointer approach.. one at the left and other at the right
         int n = height.size();
+        
         int maxArea = INT_MIN;
-        // set the end points
+        
         int left = 0, right = n - 1;
+        
         while (left < right) {
             int h = min(height[left], height[right]);
-            int width = right - left;
-            int area = h * width;
+            int w = right - left;
+            
+            int area = h * w;
+            
             maxArea = max(area, maxArea);
-            if (height[left] < height[right]) { // to maximize the area
-                left++;
-            } else if (height[left] > height[right]) { // to maximize the area
-                right--;
-            } else {
-                left++;
-                right--;
-            }
+            
+            if (height[left] < height[right]) left++;
+            else right--;
         }
         return maxArea;
     }
