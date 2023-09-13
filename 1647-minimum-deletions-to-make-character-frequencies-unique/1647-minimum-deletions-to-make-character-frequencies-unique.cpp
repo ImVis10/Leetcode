021a4fc26256ccbe40fb4058d2065099d9052e1e
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-09-13 08:19:32 -0400

    Time: 143 ms (9.84%), Space: 17.7 MB (12.64%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -11,7 +11,7 @@ public:
             if (i < s.length() && s[i] == s[i - 1]) {
                 cnt++; // frequency of a specific char until that point of time
             } else { // if it's a different char from the previous char
-                while (set.count(cnt)) {
+                while (set.count(cnt)) { // as long as a different char with same freq is present
                     if (cnt != 0) {
                         cnt--;
                         res++;
@@ -20,7 +20,7 @@ public:
                 if (cnt > 0) {
                     set.insert(cnt);
                 }
-                cnt = 1;
+                cnt = 1; // reset the frequency to 1 so that it can be used by the new char in the right way
             }
         }
         return res;
