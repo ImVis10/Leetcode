b982e54e19488ca9ae4b7baf2fe14fabdb5bebe4
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-09-21 14:40:46 -0400

    Time: 15 ms (61.12%), Space: 12.1 MB (63.55%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.cpp
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.cpp
@@ -5,16 +5,16 @@ public:
         
         priority_queue<int, vector<int>, greater<int>> pq;
         
-        int res = 0;
+        int rooms = 0;
         
         for (int i = 0; i < intervals.size(); i++) {
-            if (pq.empty() or pq.top() > intervals[i][0]) {
-                res++;
+            if (pq.empty() or pq.top() > intervals[i][0]) { // allocate new room
+                rooms++;
             } else {
                 pq.pop();
             }
             pq.push(intervals[i][1]);
         }
-        return res;
+        return rooms;
     }
 };
