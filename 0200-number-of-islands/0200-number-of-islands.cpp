891b75486b54eb7e4d46a301f4b1f1b4a33e2b7f
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-10-11 14:39:20 -0400

    Time: 219 ms (5.00%), Space: 51.9 MB (5.01%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -12,7 +12,10 @@ public:
             for (int j = 0; j < n; j++) {
                 if (!visited[i][j] && grid[i][j] == '1') {
                     res++;
-                    bfs(i, j, visited, grid);
+                    /***************BFS*****************/
+                    // bfs(i, j, visited, grid); // BFS
+                    /***************DFS*****************/
+                    dfs(i, j, visited, grid); // DFS
                 }
             }
         }
@@ -20,27 +23,40 @@ public:
     }
     
 private:
-    void bfs(int row, int col, vector<vector<bool>>& visited, vector<vector<char>>& grid) {
-        visited[row][col] = true;
-        queue<pair<int, int>> q;
-        q.push({row, col});
+//     void bfs(int row, int col, vector<vector<bool>>& visited, vector<vector<char>>& grid) {
+//         visited[row][col] = true;
+//         queue<pair<int, int>> q;
+//         q.push({row, col});
         
-        while (!q.empty()) {
-            int row = q.front().first;
-            int col = q.front().second;
-            q.pop();
+//         while (!q.empty()) {
+//             int row = q.front().first;
+//             int col = q.front().second;
+//             q.pop();
             
-            // push the neighbors onto the queue
-            vector<vector<int>> dir = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
-            for (int i = 0; i < dir.size(); i++) {
+//             // push the neighbors onto the queue
+//             vector<vector<int>> dir = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+//             for (int i = 0; i < dir.size(); i++) {
+//                 int neighRow = row + dir[i][0];
+//                 int neighCol = col + dir[i][1];
+//                 if (neighRow >= 0 && neighRow < grid.size() && neighCol >= 0 && neighCol < grid[0].size() && grid[neighRow][neighCol] == '1' && !visited[neighRow][neighCol]) {
+//                     visited[neighRow][neighCol] = true;
+//                     q.push({neighRow, neighCol});
+//                 }
+//             }
+            
+//         }
+//     }
+    void dfs(int row, int col, vector<vector<bool>>& visited, vector<vector<char>>& grid) {
+        visited[row][col] = true;
+        
+        vector<vector<int>> dir = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+        
+        for (int i = 0; i < dir.size(); i++) {
                 int neighRow = row + dir[i][0];
                 int neighCol = col + dir[i][1];
                 if (neighRow >= 0 && neighRow < grid.size() && neighCol >= 0 && neighCol < grid[0].size() && grid[neighRow][neighCol] == '1' && !visited[neighRow][neighCol]) {
-                    visited[neighRow][neighCol] = true;
-                    q.push({neighRow, neighCol});
-                }
-            }
-            
+                    dfs(neighRow, neighCol, visited, grid);
+                } 
         }
     }
 };
