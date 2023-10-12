45546fae5ad3de7773976d110facc05803c3f2b8
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-10-12 19:42:40 -0400

    Time: 10 ms (81.20%), Space: 8.1 MB (51.58%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        int m = s.length();
-        int n = t.length();
+        int m = s.length(), n = t.length();
         
         if (m < n) return "";
         
-        unordered_map<char, int> map;
+        unordered_map<char, int> freqT;
         
-        for (auto& ch : t) {
-            map[ch]++;
+        for (char ch : t) {
+            freqT[ch]++;
         }
         
-        int foundCharsFromT = 0, validMinWindowLen = INT_MAX, moveStart = 0;
+        int charsFromT = 0, minWindow = INT_MAX, windowStartsAt = 0;
         for (int i = 0, j = 0; j < m; j++) {
-            if (--map[s[j]] >= 0) foundCharsFromT++;
-            while (foundCharsFromT == n) {
-                if (j - i + 1 < validMinWindowLen) {
-                    validMinWindowLen = j - i + 1;
-                    moveStart = i;
+            if (--freqT[s[j]] >= 0) {
+                charsFromT++;
+            }
+            while (charsFromT == n) {
+                if (j - i + 1 < minWindow) {
+                    minWindow = j - i + 1;
+                    windowStartsAt = i;
                 }
-                map[s[i]]++;
-                if (map[s[i++]] > 0) foundCharsFromT--; // > 0 because only then we're sure that we found char from T in S.
+                // the next 2 lines of code are to find the minimize the window i.e., to shrink the window
+                // freqT[s[i]]++;
+                if (++freqT[s[i++]] > 0) charsFromT--; // count of chars not from t would be negative, if it's greater than 0, it means that the characters are from t, so we remove 1 from them, as  
             }
         }
-        return validMinWindowLen != INT_MAX ? s.substr(moveStart, validMinWindowLen) : "";
-        // since it's a minimum window, the window must shrink
+        return minWindow == INT_MAX ? "" : s.substr(windowStartsAt, minWindow);
     }
 };
