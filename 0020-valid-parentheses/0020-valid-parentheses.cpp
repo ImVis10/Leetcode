81d1804707c1362427408f3f84ea4b2cdc3f1297
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-10-12 19:56:54 -0400

    Time: 3 ms (44.81%), Space: 6.6 MB (39.80%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -2,11 +2,14 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char> stack;
-        
-        for (auto& ch : s) {
-            if (stack.empty()) stack.push(ch);
-            else if (stack.top() == '(' && ch == ')' || stack.top() == '{' && ch == '}' || stack.top() == '[' && ch == ']') stack.pop();
-            else stack.push(ch);
+        for (char ch : s) {
+            if (stack.empty()) {
+                stack.push(ch);
+            } else if (stack.top() == '(' && ch == ')' || stack.top() == '{' && ch == '}' || stack.top() == '[' && ch == ']') {
+                stack.pop();
+            } else {
+                stack.push(ch);
+            }
         }
         return stack.empty();
     }
