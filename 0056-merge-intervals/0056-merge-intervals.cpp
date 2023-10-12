c49c2764b3080439ce2fa34d254b2af4862a8eaf
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-10-12 11:49:20 -0400

    Time: 21 ms (96.09%), Space: 19.4 MB (37.87%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        vector<vector<int>> res;
-        
         int n = intervals.size();
         
         sort(intervals.begin(), intervals.end());
+        
+        vector<vector<int>> res;
         
         for (int i = 0; i < n; i++) {
             if (res.empty() || intervals[i][0] > res.back()[1]) {
                 res.push_back(intervals[i]);
             } else {
-                res.back()[1] = max(intervals[i][1], res.back()[1]);
+                res.back()[1] = max(res.back()[1], intervals[i][1]);
             }
         }
         return res;
