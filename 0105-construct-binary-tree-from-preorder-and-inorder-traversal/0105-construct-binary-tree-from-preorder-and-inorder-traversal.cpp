4e61973c4b870c356a66a6cf45d7e4ebe4323606
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-10-13 11:45:37 -0400

    Time: 33 ms (22.04%), Space: 26 MB (78.16%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -11,16 +11,19 @@
  */
 class Solution {
 public:
-TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) { 
+        // preorder - to find out which node is the root
+        // inorder - to find out the left and right subtrees
         int idx = 0;
-        return build(preorder, inorder, 0, inorder.size() - 1, idx);
+        return build(preorder, inorder, 0, preorder.size() - 1, idx);
     }
 private:
     TreeNode* build(vector<int>& preorder, vector<int>& inorder, int start, int end, int& idx) {
+        // start, end - pass by value as it is specific to left and right values
+        // idx - pass by reference as it needs to be updated across all the calls because it keeps track of the position in preorder vector, which is a shared state across all calls
         if (start > end) return NULL;
         
         TreeNode* root = new TreeNode(preorder[idx]);
-        
         int splitAt = 0;
         
         for (int i = 0; i < inorder.size(); i++) {
@@ -29,8 +32,7 @@ private:
                 break;
             }
         }
-        idx++;
-        
+        idx++; // prepare for the next node of the tree to place it at the appropriate position
         root->left = build(preorder, inorder, start, splitAt - 1, idx);
         root->right = build(preorder, inorder, splitAt + 1, end, idx);
         return root;
