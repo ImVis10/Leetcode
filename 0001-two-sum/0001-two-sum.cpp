a06b4296c20c428a1c2a4d242acad7e8ae189348
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-10-16 18:41:34 -0400

    Time: 7 ms (90.00%), Space: 11.2 MB (22.80%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,10 +3,12 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n = nums.size();
         unordered_map<int, int> mp;
+        
         for (int i = 0; i < n; i++) {
-            int secondNum = target - nums[i];
-            if (mp.count(secondNum)) return {mp[secondNum], i};
-            mp[nums[i]] = i;
+            int firstNum = nums[i];
+            int secondNum = target - firstNum;
+            if (mp.find(secondNum) != mp.end()) return {mp[secondNum], i};
+            mp[firstNum] = i;
         }
         return {};
     }
