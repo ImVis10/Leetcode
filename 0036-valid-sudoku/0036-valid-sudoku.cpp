58beb978732d180234fd4452807b7bb9890897a6
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-10-16 20:03:09 -0400

    Time: 18 ms (58.93%), Space: 20.2 MB (36.58%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,21 +1,61 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        vector<set<int>> row(9), col(9), block(9);
+        // to check 3 things
+        // 1. if the row is valid
+        // 2. if the column is valid
+        // 3. if the sub squares are valid
+        
         for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                if (board[i][j] != '.') {
-                    int val = board[i][j] - '0';
-                    if (row[i].count(val) || col[j].count(val) || block[(i / 3) * 3 + j / 3].count(val)) { // using count to check the presence of the element in the set, if present returns 1
-                        return false;
-                    }
-                    row[i].insert(val);
-                    col[j].insert(val);
-                    block[(i / 3) * 3 + j / 3].insert(val);
+            bool rowCheck = isRowValid(board, i);
+            if (!rowCheck) return false;
+            
+            bool colCheck = isColumnValid(board, i);
+            if (!colCheck) return false;
+            
+            bool subSquareCheck = isSubSquareValid(board, i);
+            if (!subSquareCheck) return false;
+        }
+        return true;
+    }
+private:
+    bool isRowValid(vector<vector<char>>& board, int row) {
+        set<char> st;
+        for (char ch : board[row]) {
+            if (ch != '.') {
+                if (st.find(ch) != st.end()) return false;
+                st.insert(ch);
+            }
+        }
+        return true;
+    }
+    
+    bool isColumnValid(vector<vector<char>>& board, int col) {
+        set<char> st;
+        for (int i = 0; i < 9; i++) {
+            char ch = board[i][col];
+            if (ch != '.') {
+                if (st.find(ch) != st.end()) return false;
+                st.insert(ch);
+            }
+        }
+        return true;
+    }
+    
+    bool isSubSquareValid(vector<vector<char>>& board, int box) {
+        set<char> st;
+        
+        int startRow = (box / 3) * 3;
+        int startCol = (box % 3) * 3;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                char ch = board[startRow + i][startCol + j];
+                if (ch != '.') {
+                    if (st.find(ch) != st.end()) return false;
+                    st.insert(ch);
                 }
             }
         }
         return true;
-
     }
 };
