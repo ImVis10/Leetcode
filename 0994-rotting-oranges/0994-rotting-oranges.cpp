662241bf85d505aa8eeed87255e7ad473aadf5b0
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-10-17 17:01:27 -0400

    Time: 11 ms (12.69%), Space: 13.5 MB (32.86%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,37 +1,37 @@
 class Solution {
+    vector<vector<int>> dirs = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
 public:
     int orangesRotting(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        
+        int fresh = 0;
+        
         queue<pair<int, int>> q;
-        int time = 0, fresh = 0;
         
-        int nRows = grid.size();
-        int nCols = grid[0].size();
-        
-        for (int i = 0; i < nRows; i++) {
-            for (int j = 0; j < nCols; j++) {
-                if (grid[i][j] == 1) {
-                    fresh++;
-                } else if (grid[i][j] == 2) {
-                    q.push({i, j});
-                }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) fresh++;
+                else if (grid[i][j] == 2) q.push({i, j}); 
             }
         }
-        
-        
-        
-        while (!q.empty() && fresh > 0) {
+        int time = bfs(grid, fresh, q);
+        return time;
+    }
+private:
+    int bfs(vector<vector<int>>& grid, int fresh, queue<pair<int, int>>& q) {
+        int m = grid.size(), n = grid[0].size();
+        int time = 0;
+        while (!q.empty() && fresh) {
             int sz = q.size();
             for (int i = 0; i < sz; i++) {
                 auto cell = q.front();
                 q.pop();
-                for (int d = 0; d < dirs.size(); d++) {
-                    int toRow = dirs[d][0] + cell.first;
-                    int toCol = dirs[d][1] + cell.second;
-                    if (toRow < 0 || toRow >= nRows || toCol < 0 || toCol >= nCols ||grid[toRow][toCol] != 1) {
-                        continue;
-                    }
-                    grid[toRow][toCol] = 2;
-                    q.push({toRow, toCol});
+                for (auto& dir : dirs) {
+                    int rowTo = cell.first + dir[0];
+                    int colTo = cell.second + dir[1];
+                    if (rowTo < 0 || rowTo >= m || colTo < 0 || colTo >= n || grid[rowTo][colTo] != 1) continue;
+                    grid[rowTo][colTo] = 2;
+                    q.push({rowTo, colTo});
                     fresh--;
                 }
             }
@@ -39,6 +39,4 @@ public:
         }
         return fresh == 0 ? time : -1;
     }
-private:
-    vector<vector<int>> dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 };
