f449965828a9d10b6b523ba6685f3506adf17332
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-10-17 13:50:30 -0400

    Time: 15 ms (19.46%), Space: 10.9 MB (23.10%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -4,35 +4,36 @@ public:
     void solve(vector<vector<char>>& board) {
         int m = board.size(), n = board[0].size();
         
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
+        
         for (int row = 0; row < m; row++) {
-            dfs(row, 0, board);
-            dfs(row, n - 1, board);
+            dfs(row, 0, board, visited);
+            dfs(row, n - 1, board, visited);
         }
         
         for (int col = 0; col < n; col++) {
-            dfs(0, col, board);
-            dfs(m-1, col, board);
+            dfs(0, col, board, visited);
+            dfs(m-1, col, board, visited);
         }
         
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (board[i][j] == 'O') board[i][j] = 'X';
-                else if (board[i][j] == 'D') board[i][j] = 'O';
+                if (!visited[i][j]) board[i][j] = 'X';
             }
         }  
     }
 private:
-    void dfs(int row, int col, vector<vector<char>>& board) {
-        if (row < 0 || row >= board.size() || col < 0 || col >= board[0].size() || board[row][col] != 'O') {
+    void dfs(int row, int col, vector<vector<char>>& board, vector<vector<bool>>& visited) {
+        if (row < 0 || row >= board.size() || col < 0 || col >= board[0].size() || visited[row][col] || board[row][col] != 'O') {
             return;
         }
         
-        board[row][col] = 'D';
+        visited[row][col] = true;
         
         for (auto& d : dir) {
             int rowTo = row + d[0];
             int colTo = col + d[1];
-            dfs(rowTo, colTo, board);
+            dfs(rowTo, colTo, board, visited);
         }
     }
 };
