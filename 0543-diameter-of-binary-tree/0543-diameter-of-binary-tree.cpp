51c6ad8498929aaab2a4a27b9b850dde45d3b14d
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-10-18 22:26:01 -0400

    Time: 9 ms (49.02%), Space: 20.5 MB (55.37%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -10,23 +10,18 @@
  * };
  */
 class Solution {
-    int diameter = 0;
 public:
     int diameterOfBinaryTree(TreeNode* root) {
-       // diameter of every node would be would be the sum of its left subtree and right subtree height
-        height(root);
+        int diameter = 0;
+        height(root, diameter);
         return diameter;
-        
     }
 private:
-    int height(TreeNode* node) {
-        if (node == NULL) return 0;
-        
-        int lh = height(node->left);
-        int rh = height(node->right);
-        
-        diameter = max(diameter, lh + rh); // diameter can not only be through root as the the nodes with the max distance between them can be on the same side of the root. So storing the diameter value as well.
-        
+    int height(TreeNode* root, int& diameter) {
+        if (!root) return 0;
+        int lh = height(root->left, diameter);
+        int rh = height(root->right, diameter);
+        diameter = max(diameter, lh + rh);
         return 1 + max(lh, rh);
     }
 };
