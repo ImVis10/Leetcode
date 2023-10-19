88797ccd7f79153cebc143505d021031c2248892
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-10-18 21:11:41 -0400

    Time: 0 ms (100.00%), Space: 9.9 MB (56.04%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.cpp
+++ 0226-invert-binary-tree/0226-invert-binary-tree.cpp
@@ -12,13 +12,10 @@
 class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
-        if (root == NULL) return NULL;
-        
+        if (!root) return NULL;
         swap(root->left, root->right);
-        
         invertTree(root->left);
         invertTree(root->right);
-        
         return root;
     }
 };
