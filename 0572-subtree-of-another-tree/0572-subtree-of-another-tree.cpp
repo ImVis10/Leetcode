8467aa2564c76d9d03481e72fc0b25717903d71e
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-10-18 22:35:16 -0400

    Time: 18 ms (60.41%), Space: 29.1 MB (32.29%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
@@ -12,18 +12,15 @@
 class Solution {
 public:
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
-        if (root == NULL) return false;
-        
+        if (!root) return false;
         if (isSameTree(root, subRoot)) return true;
         
         return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
     }
-    
 private:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if (p == NULL && q == NULL) return true;
-        if (p == NULL || q == NULL) return false;
-        
+        if (!p && !q) return true;
+        if (!p || !q) return false;
         return (p->val == q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right));
     }
 };
