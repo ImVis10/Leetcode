bc4f85f955950c82cade13288770726a38d9d94c
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-10-23 12:43:21 -0400

    Time: 38 ms (32.19%), Space: 13.5 MB (96.76%) - LeetHub

--- 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
@@ -6,10 +6,11 @@ public:
         int n = colors.length();
         
         for (int i = 1; i < n - 1; i++) {
-            if (colors[i] == 'A' && colors[i] == colors[i - 1] && colors[i] == colors[i + 1]) alice++;
-            else if (colors[i] == 'B' && colors[i] == colors[i - 1] && colors[i] == colors[i + 1]) bob++;
+            if (colors[i] == colors[i - 1] && colors[i] == colors[i + 1]) {
+                if (colors[i] == 'A') alice++;
+                else bob++;
+            }
         }
-        
         return bob < alice;
     }
 };
