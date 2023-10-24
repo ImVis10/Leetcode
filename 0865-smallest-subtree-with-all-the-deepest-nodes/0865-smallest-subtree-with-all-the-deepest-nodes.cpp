d09441b9ed9199afb3aa406bf21a08d4b162a848
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-10-24 12:46:08 -0400

    Time: 0 ms (100.00%), Space: 14.6 MB (53.85%) - LeetHub

---
+++ 0865-smallest-subtree-with-all-the-deepest-nodes/0865-smallest-subtree-with-all-the-deepest-nodes.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* subtreeWithAllDeepest(TreeNode* root) {
+        int maxDepth = 0;
+        TreeNode* res;
+        dfs(root, 0, maxDepth, res);
+        return res;
+    }
+private:
+    int dfs(TreeNode* root, int depth, int& maxDepth, TreeNode* &res) {
+        if (!root) return depth;
+        
+        int lh = dfs(root->left, depth + 1, maxDepth, res);
+        int rh = dfs(root->right, depth + 1, maxDepth, res);
+        
+        int depthOfSubTree = max(lh, rh);
+        maxDepth = max(maxDepth, depthOfSubTree);
+        
+        if (lh == rh && lh == maxDepth) res = root; // key statement of the logic
+        return depthOfSubTree;
+    }
+};
