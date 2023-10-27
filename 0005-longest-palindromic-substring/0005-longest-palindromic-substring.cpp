66bf412ea273aab21d4fe28f92634d376b211c8d
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-10-27 12:14:29 -0400

    Time: 690 ms (21.98%), Space: 23.5 MB (32.27%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int n = s.length();
+        
+        vector<vector<bool>> cache(n, vector<bool>(n));
+        
+        int maxLen = 0, start = 0;
+        
+        string res = "";
+        
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i; j < n; j++) {
+                cache[i][j] = s[i] == s[j] && (j - i < 3 || cache[i + 1][j -1]);
+                
+                if (cache[i][j] && j - i + 1 > maxLen) {
+                    start = i;
+                    maxLen = j - i + 1;
+                }
+            }
+        }
+        return s.substr(start, maxLen);
+    }
+};
