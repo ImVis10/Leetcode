e3ac7d82efcfda106d37437841ee084e3c496ba1
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-11-13 08:30:56 -0500

    Time: 82 ms (10.25%), Space: 13.8 MB (39.29%) - LeetHub

--- 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.cpp
+++ 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.cpp
@@ -2,26 +2,26 @@ class Solution {
 public:
     string sortVowels(string s) {
         vector<char> vowels;
-        for (auto& ch : s) {
-            if (isVowel(ch)) vowels.push_back(ch);
+        
+        for (char ch : s) {
+            if (string("aeiouAEIOU").find(ch) != string::npos) {
+                vowels.push_back(ch);
+            }
         }
+        
         sort(vowels.begin(), vowels.end());
         reverse(vowels.begin(), vowels.end());
         
-        string res = "";
-        for (int  i = 0; i < s.length(); i++) {
-            if (isVowel(s[i])) {
+        string res;
+        
+        for (char ch : s) {
+            if (string("aeiouAEIOU").find(ch) != string::npos) {
                 res += vowels.back();
                 vowels.pop_back();
             } else {
-                res += s[i];
+                res += ch;
             }
         }
         return res;
-    }
-private:
-    bool isVowel (char x) {
-	    char y = toupper(x);
-	    return (y =='A' || y =='E' || y =='I'|| y =='O'|| y =='U');
     }
 };
