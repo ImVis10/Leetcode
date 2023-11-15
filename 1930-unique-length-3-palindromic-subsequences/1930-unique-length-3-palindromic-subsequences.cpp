5da9e2d0e4f71a936a5e0e3a56a1eff871e7c95d
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-11-14 19:40:27 -0500

    Time: 683 ms (29.76%), Space: 31.2 MB (17.21%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
@@ -5,24 +5,16 @@ public:
         unordered_map<char, pair<int, int>> mp;
         
         for (int i = 0; i < n; i++) {
-            if (mp.find(s[i]) != mp.end()) {
-                mp[s[i]].second = i;  
-            } else {
-                mp[s[i]] = {i, -1};
-            }
+            if (mp.find(s[i]) != mp.end()) mp[s[i]].second = i;
+            else mp[s[i]] = {i, -1};
         }
         
         int res = 0;
-        
-        // for (auto)
-        
+            
         for (auto& [k, v] : mp) {
-            // cout << k << endl;
             int start = v.first, end = v.second;
-            // cout << start << " " << end << endl;
             if (end == -1) continue;
             string between = s.substr(start + 1, end - start - 1);
-            cout << between << endl;
             set<char> set = makeSet(between);
             res += set.size();
         }
