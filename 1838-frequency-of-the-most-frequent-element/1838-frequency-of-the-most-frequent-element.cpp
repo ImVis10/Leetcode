6708a4284cea0f8976e5ea04bdc3d9c2e346a312
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-11-18 14:10:08 -0500

    Time: 161 ms (83.71%), Space: 99.3 MB (85.20%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
@@ -6,13 +6,13 @@ public:
         sort(nums.begin(), nums.end());
         
         int res = 1;
-        long long cnt = 0;
+        long long sum = 0;
         
         for (int low = 0, high = 0; high < n; high++) {
-            cnt += nums[high];
+            sum += nums[high];
             // Check if the count of increments needed to make all elements in the current window equal to nums[high] is more than k
-            while (static_cast<long long> (nums[high]) * (high - low + 1) - cnt > k) {
-                cnt -= nums[low++];
+            while (static_cast<long long> (nums[high]) * (high - low + 1) - sum > k) {
+                sum -= nums[low++];
             }
             res = max(res, high - low + 1);
         }
