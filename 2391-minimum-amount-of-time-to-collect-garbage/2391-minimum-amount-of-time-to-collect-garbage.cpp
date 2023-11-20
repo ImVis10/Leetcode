54ca4e2561d3c9c20a76209b3a51a849291659b2
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-11-19 21:44:30 -0500

    Time: 221 ms (8.30%), Space: 102.6 MB (19.78%) - LeetHub

---
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int garbageCollection(vector<string>& garbage, vector<int>& travel) {
+        int n = garbage.size();
+        
+        unordered_map<char, int> truckTravelsTill;
+        
+        for (int i = 0; i < n; i++) {
+            if (garbage[i].find('M') != string::npos) truckTravelsTill['M'] = i;
+            if (garbage[i].find('G') != string::npos) truckTravelsTill['G'] = i;
+            if (garbage[i].find('P') != string::npos) truckTravelsTill['P'] = i;
+        }
+        
+        for (auto& [k, v] : truckTravelsTill) {
+            cout << k << "  " << v << endl;
+        }
+        
+        int time = 0;
+        
+        for (auto& [k, v] : truckTravelsTill) {
+            cout << time << endl;
+            for (int i = 0; i <= v; i++) {
+                for (char ch : garbage[i]) {
+                    if (ch == k)  {
+                        time++;
+                    }
+                }
+                if (i != v) time += travel[i];
+                // cout << time << endl;
+            }
+        }
+        cout << time << endl;
+        return time;
+    }
+};
