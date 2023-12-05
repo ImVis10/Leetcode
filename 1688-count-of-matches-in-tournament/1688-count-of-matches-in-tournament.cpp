f203f889a7c6c47bfb6146802df1ebe2286cb322
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-12-04 19:14:48 -0500

    Time: 0 ms (100.00%), Space: 6.5 MB (13.68%) - LeetHub

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     int numberOfMatches(int n) {
-        int res = 0;
-        while (n >= 2) {
-            res += n / 2;
-            n = n % 2 == 0 ? n / 2 : ((n - 1) / 2) + 1;
-        }
-        return res;
+        if (n <= 1) return 0;
+        
+        if (n % 2 == 0) return n / 2 + numberOfMatches(n / 2);
+        
+        else return n / 2 + numberOfMatches((n / 2) + 1);
     }
 };
