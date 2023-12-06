93b29dceccf281e44af144946b25c3a7133f7c42
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-12-06 18:43:47 -0500

    Time: 98 ms (46.52%), Space: 27.6 MB (34.76%) - LeetHub

--- 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.cpp
+++ 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.cpp
@@ -1,37 +1,28 @@
 class Solution {
 public:
     bool canBeValid(string s, string locked) {
-        if (s.length() & 1) return false;
+        int n = s.length();
+        
+        if (n % 2) return false;
         
         int canFlip = 0, open = 0, close = 0;
         
-        for (int i = 0; i < s.length(); i++) {
-            if (locked[i] == '0') {
-                canFlip++;
-            } else if (s[i] == '(') {
-                open++;
-            } else {
-                close++;
-            }
-            if (canFlip < close - open) {
-                return false;
-            }
+        for (int i = 0; i < n; i++) {
+            if (locked[i] == '0') canFlip++;
+            else if (s[i] == '(') open++;
+            else close++;
+            if (close > canFlip + open) return false;
         }
-                
+        
         canFlip = 0, open = 0, close = 0;
         
-        for (int i = s.length() - 1; i >= 0; i--) {
-            if (locked[i] == '0') {
-                canFlip++;
-            } else if (s[i] == '(') {
-                open++;
-            } else {
-                close++;
-            }
-            if (canFlip < open - close) {
-                return false;
-            }
+        for (int i = n - 1; i >= 0; i--) {
+            if (locked[i] == '0') canFlip++;
+            else if (s[i] == '(') open++;
+            else close++;
+            if (open > canFlip + close) return false;
         }
+        
         return true;
     }
 };
