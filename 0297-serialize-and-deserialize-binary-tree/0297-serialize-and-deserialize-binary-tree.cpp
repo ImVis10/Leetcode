73c9ac40510af0a7716fe7656237ea94a7f503fe
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-12-09 18:36:15 -0500

    Time: 32 ms (51.07%), Space: 29.3 MB (90.36%) - LeetHub

--- 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.cpp
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.cpp
@@ -13,6 +13,7 @@ public:
     // Encodes a tree to a single string.
     string serialize(TreeNode* root) {
         string res = "";
+        
         queue<TreeNode*> q;
         q.push(root);
         
@@ -21,69 +22,51 @@ public:
             for (int i = 0; i < levelSize; i++) {
                 TreeNode* node = q.front();
                 q.pop();
-                
-                if (node) {
+                if (!node) res += "#,";
+                else {
                     res += to_string(node->val) + ',';
                     q.push(node->left);
                     q.push(node->right);
-                } else {
-                    res += "#,";
                 }
-            }     
+            }
         }
-        res.pop_back();
-        cout <<  res << endl;
+        res.pop_back(); // remove the extra ','
+        cout << res;
         return res;
     }
 
     // Decodes your encoded data to tree.
     TreeNode* deserialize(string data) {
         if (data.empty() || data == "#") return NULL;
-        
         stringstream ss(data);
-        string item;
+        string node;
         
-        getline(ss, item, ','); // get a char separated by delimiter ','
-        TreeNode* root = new TreeNode(stoi(item));
+        getline(ss, node, ','); // get each node with , being the delimiter
+        
+        TreeNode* root = new TreeNode(stoi(node));
         
         queue<TreeNode*> q;
         q.push(root);
         
         while (!q.empty()) {
-            TreeNode* node = q.front();
+            TreeNode* currNode = q.front();
             q.pop();
             
-            if (getline(ss, item, ',')) {
-                if (item != "#") {
-                    node->left = new TreeNode(stoi(item));
-                    q.push(node->left);
-                }
+            getline(ss, node, ',');
+            if (node != "#") {
+                currNode->left = new TreeNode(stoi(node));
+                q.push(currNode->left);
             }
             
-            if (getline(ss, item, ',')) {
-                if (item != "#") {
-                    node->right = new TreeNode(stoi(item));
-                    q.push(node->right);
-                }
+            getline(ss, node, ',');
+            if (node != "#") {
+                currNode->right = new TreeNode(stoi(node));
+                q.push(currNode->right);
             }
+            
+            
         }
-        
-        return root;
-    }
-
-private:
-    vector<char> splitString(string s) {
-        vector<char> res;
-        stringstream ss(s);
-        
-        string item;
-        
-        while (getline(ss, item, ',')) {
-            if (item.size() == 1) {
-                res.push_back(item[0]);                
-            }
-        }
-        return res;
+        return root;   
     }
 };
 
