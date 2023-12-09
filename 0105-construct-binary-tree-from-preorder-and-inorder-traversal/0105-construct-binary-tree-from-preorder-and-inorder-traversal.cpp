40cecf2aae1f2a26f0534c1749c449a7fb9e9fe1
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-12-09 18:08:44 -0500

    Time: 52 ms (5.82%), Space: 26.1 MB (91.43%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -11,30 +11,30 @@
  */
 class Solution {
 public:
-    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) { 
-        // preorder - to find out which node is the root
-        // inorder - to find out the left and right subtrees
-        int idx = 0;
-        return build(preorder, inorder, 0, preorder.size() - 1, idx);
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        int curr = 0;
+        return build(preorder, inorder, curr, 0, inorder.size() - 1);
     }
 private:
-    TreeNode* build(vector<int>& preorder, vector<int>& inorder, int start, int end, int& idx) {
-        // start, end - pass by value as it is specific to left and right values
-        // idx - pass by reference as it needs to be updated across all the calls because it keeps track of the position in preorder vector, which is a shared state across all calls
+    TreeNode* build(vector<int>& preorder, vector<int>& inorder, int& curr, int start, int end) {
         if (start > end) return NULL;
         
-        TreeNode* root = new TreeNode(preorder[idx]);
-        int splitAt = 0;
+        int splitPos = 0;
         
-        for (int i = 0; i < inorder.size(); i++) {
-            if (preorder[idx] == inorder[i]) {
-                splitAt = i;
+        TreeNode* root = new TreeNode(preorder[curr]);
+        
+        for (int i = 0; i < preorder.size(); i++) {
+            if (inorder[i] == preorder[curr]) {
+                splitPos = i;
                 break;
             }
         }
-        idx++; // prepare for the next node of the tree to place it at the appropriate position
-        root->left = build(preorder, inorder, start, splitAt - 1, idx);
-        root->right = build(preorder, inorder, splitAt + 1, end, idx);
+        
+        curr++; // root is set in its position, now move forward to build the rest of the tree
+        
+        // divide and conquer
+        root->left = build(preorder, inorder, curr, start, splitPos - 1);
+        root->right = build(preorder, inorder, curr, splitPos + 1, end); 
         return root;
     }
 };
