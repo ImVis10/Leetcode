c2d3a979f07955b9b9f84b171beec951a6854e4c
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-12-09 11:49:44 -0500

    Time: 1756 ms (52.66%), Space: 0B (100.00%) - LeetHub

--- 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -1,7 +1,10 @@
 # Write your MySQL query statement below
+WITH YearlySales AS (
+    SELECT product_id, year, quantity, price,
+    RANK() OVER(PARTITION BY product_id ORDER BY year) as salesByYear
+    FROM Sales
+)
+
 SELECT product_id, year AS first_year, quantity, price 
-FROM 
-    (SELECT product_id, year, quantity, price,
-    RANK() OVER(PARTITION BY product_id ORDER BY year) AS salesByYearForEachProduct
-    FROM Sales) AS innerQuery
-WHERE salesByYearForEachProduct = 1
+FROM YearlySales
+WHERE salesByYear = 1
