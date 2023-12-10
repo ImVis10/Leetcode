4979bdd8406d639dc96b9cdc3314392f9d60548a
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-12-10 18:55:45 -0500

    Time: 38 ms (21.65%), Space: 18.8 MB (24.96%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,4 +1,5 @@
 class Solution {
+    vector<vector<int>> dirs = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
 public:
     int numIslands(vector<vector<char>>& grid) {
         int m = grid.size(), n = grid[0].size();
@@ -10,7 +11,8 @@ public:
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == '1' && !visited[i][j]) {
                     res++;
-                    dfs(i, j, grid, visited);
+                    // dfs(i, j, grid, visited);
+                    bfs(i, j, grid, visited);
                 }
             }
         }
@@ -28,6 +30,27 @@ private:
             
             if (rowTo >= 0 && rowTo < grid.size() && colTo >= 0 && colTo < grid[0].size() && !visited[rowTo][colTo] && grid[rowTo][colTo] == '1') {
                 dfs(rowTo, colTo, grid, visited);
+            }
+        }
+    }
+    
+    void bfs(int row, int col, vector<vector<char>>& grid, vector<vector<bool>>& visited) {
+        visited[row][col] = true;
+        
+        queue<pair<int, int>> q;
+        q.push({row, col});
+        
+        while (!q.empty()) {
+            int row = q.front().first;
+            int col = q.front().second;
+            q.pop();
+            for (auto& dir : dirs) {
+                int rowTo = row + dir[0];
+                int colTo = col + dir[1];
+                if (rowTo >= 0 and rowTo < grid.size() and colTo >= 0 and colTo < grid[0].size() and !visited[rowTo][colTo] and grid[rowTo][colTo] == '1') {
+                    visited[rowTo][colTo] = true;
+                    q.push({rowTo, colTo});
+                }
             }
         }
     }
