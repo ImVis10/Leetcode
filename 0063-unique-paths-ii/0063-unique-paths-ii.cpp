d1cd10cf9de41f7613e5094c1ed997c0052fe7e4
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-12-12 22:06:21 -0500

    Time: 2 ms (60.27%), Space: 8.1 MB (45.35%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -2,22 +2,18 @@ class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m = obstacleGrid.size(), n = obstacleGrid[0].size();
-        vector<vector<int>> cache(m, vector<int>(n , -1));
-        return recurse(0, 0, m - 1, n - 1, obstacleGrid, cache);
+        vector<vector<int>> cache(m, vector<int> (n, -1));
+        return recurse(0, 0, m, n, cache, obstacleGrid);
     }
 private:
-    int recurse(int currRow, int currCol, int destRow, int destCol, vector<vector<int>>& grid, vector<vector<int>>& cache) {
-        if (currRow < destRow + 1 && currCol < destCol + 1 && grid[currRow][currCol] == 1) return 0;
-
-        if (currRow == destRow && currCol == destCol) return 1;
-        if (currRow > destRow || currCol  > destCol) return 0;
+    int recurse(int i, int j, int m, int n, vector<vector<int>>& cache, vector<vector<int>>& grid) {
+        if (i == m - 1 and j == n - 1 and grid[i][j] == 0) return 1;
+        if (i > m - 1 or j > n - 1 or grid[i][j] == 1) return 0;
+        if (cache[i][j] != -1) return cache[i][j];
         
+        int right = recurse(i, j + 1, m, n, cache, grid);
+        int down = recurse(i + 1, j, m, n, cache, grid);
         
-        if (cache[currRow][currCol] != -1) return cache[currRow][currCol];
-        
-        int down = recurse(currRow + 1, currCol, destRow, destCol, grid, cache);
-        int right = recurse(currRow, currCol + 1, destRow, destCol, grid, cache);
-        
-        return cache[currRow][currCol] = down + right;
+        return cache[i][j] = right + down;
     }
 };
