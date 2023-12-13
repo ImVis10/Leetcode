67f22053afdf5c100e908cee0ed98006610f42a6
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-12-12 21:59:16 -0500

    Time: 0 ms (100.00%), Space: 6.7 MB (63.22%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -6,7 +6,7 @@ public:
     }
 private:
     int recurse(int i, int j, int m, int n, vector<vector<int>>& cache) {
-        if (i== m - 1 and j == n - 1) return 1;
+        if (i == m - 1 and j == n - 1) return 1;
         if (i > m - 1 or j > n - 1) return 0;
         if (cache[i][j] != -1) return cache[i][j];
         
