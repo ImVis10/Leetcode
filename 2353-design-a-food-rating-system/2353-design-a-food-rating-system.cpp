8937d1a69f74a280d5a30660b716e12563dc5aa1
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-12-17 12:33:09 -0500

    Time: 377 ms (87.08%), Space: 155.8 MB (95.22%) - LeetHub

--- 2353-design-a-food-rating-system/2353-design-a-food-rating-system.cpp
+++ 2353-design-a-food-rating-system/2353-design-a-food-rating-system.cpp
@@ -1,23 +1,23 @@
 class FoodRatings {
-    unordered_map<string, int> foodRatings;
-    unordered_map<string, string> foodToCuisine;
+    unordered_map<string, pair<string , int>> foodToCusineAndRatings;
+    // unordered_map<string, string> foodToCuisine;
     unordered_map<string, set<pair<int, string>>> highestRatedFood;
 public:
     FoodRatings(vector<string>& foods, vector<string>& cuisines, vector<int>& ratings) {
         for (int i = 0; i < foods.size(); i++) {
-            foodRatings[foods[i]] = ratings[i];
-            foodToCuisine[foods[i]] = cuisines[i];
+            foodToCusineAndRatings[foods[i]] = {cuisines[i], ratings[i]};
+            // foodToCuisine[foods[i]] = cuisines[i];
             highestRatedFood[cuisines[i]].insert({-ratings[i], foods[i]});
         }
     }
     
     void changeRating(string food, int newRating) {
         // update foodRatings map
-        int oldRating = foodRatings[food];
-        foodRatings[food] = newRating; 
+        int oldRating = foodToCusineAndRatings[food].second;
+        foodToCusineAndRatings[food].second = newRating; 
         
         // find the cuisine for the food so that we can update the ratings in highestRatedFood
-        string cuisine = foodToCuisine[food];
+        string cuisine = foodToCusineAndRatings[food].first;
 
         // update the highestRatedFood with the newRating for the cuisine, food combination
         highestRatedFood[cuisine].erase({-oldRating, food});
