811c5333fa3a4e50ad892849a3c75bc5af67d8e3
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-12-17 12:28:56 -0500

    Time: 431 ms (55.98%), Space: 166.3 MB (67.46%) - LeetHub

---
+++ 2353-design-a-food-rating-system/2353-design-a-food-rating-system.cpp
@@ -0,0 +1,37 @@
+class FoodRatings {
+    unordered_map<string, int> foodRatings;
+    unordered_map<string, string> foodToCuisine;
+    unordered_map<string, set<pair<int, string>>> highestRatedFood;
+public:
+    FoodRatings(vector<string>& foods, vector<string>& cuisines, vector<int>& ratings) {
+        for (int i = 0; i < foods.size(); i++) {
+            foodRatings[foods[i]] = ratings[i];
+            foodToCuisine[foods[i]] = cuisines[i];
+            highestRatedFood[cuisines[i]].insert({-ratings[i], foods[i]});
+        }
+    }
+    
+    void changeRating(string food, int newRating) {
+        // update foodRatings map
+        int oldRating = foodRatings[food];
+        foodRatings[food] = newRating; 
+        
+        // find the cuisine for the food so that we can update the ratings in highestRatedFood
+        string cuisine = foodToCuisine[food];
+
+        // update the highestRatedFood with the newRating for the cuisine, food combination
+        highestRatedFood[cuisine].erase({-oldRating, food});
+        highestRatedFood[cuisine].insert({-newRating, food});
+    }
+    
+    string highestRated(string cuisine) {
+        return highestRatedFood[cuisine].begin()->second;
+    }
+};
+
+/**
+ * Your FoodRatings object will be instantiated and called as such:
+ * FoodRatings* obj = new FoodRatings(foods, cuisines, ratings);
+ * obj->changeRating(food,newRating);
+ * string param_2 = obj->highestRated(cuisine);
+ */
