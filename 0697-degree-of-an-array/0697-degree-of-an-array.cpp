01f8bca1183ec5b4d811a5ec4eaae9f13a5f1941
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-12-22 17:49:37 -0500

    Time: 24 ms (86.85%), Space: 28 MB (18.59%) - LeetHub

--- 0697-degree-of-an-array/0697-degree-of-an-array.cpp
+++ 0697-degree-of-an-array/0697-degree-of-an-array.cpp
@@ -2,28 +2,24 @@ class Solution {
 public:
     int findShortestSubArray(vector<int>& nums) {
         int n = nums.size();
-        unordered_map<int, vector<int>> map;
+        
+        unordered_map<int, vector<int>> mp;
+        
         for (int i = 0; i < n; i++) {
-            map[nums[i]].push_back(i);
+            mp[nums[i]].push_back(i);
         }
         
-        vector<int> maxKeys;
-        int maxCount = 0;
-        int res = INT_MAX;
+        int maxFreq = 0, res = INT_MAX;
         
-        for(auto& pair : map) {
-            if (pair.second.size() > maxCount) {
-                maxCount = pair.second.size();
+        for (auto& entry : mp) {
+            if (entry.second.size() > maxFreq) maxFreq = entry.second.size();
+        }
+                
+        for (auto& entry : mp) {
+            if (entry.second.size() == maxFreq) {
+                res = min(res, entry.second[maxFreq - 1] - entry.second[0] + 1);
             }
         }
-        
-        for (auto& pair : map) {
-            if (pair.second.size() == maxCount) {
-                if (pair.second.size() == 1) return 1;
-                res = min(res, pair.second[maxCount - 1] - pair.second[0] + 1);
-            }
-        }
-        
         return res;
     }
 };
