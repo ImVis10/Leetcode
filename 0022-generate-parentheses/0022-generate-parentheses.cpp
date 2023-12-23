a23447400033ac4c51ba68dabb8b5c552f6a0b36
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-12-23 09:08:09 -0500

    Time: 7 ms (20.46%), Space: 15.3 MB (59.67%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,27 +1,18 @@
 class Solution {
 public:
     vector<string> generateParenthesis(int n) {
-        // #opening paranthesis = n, #closing paranthesis = n
-        // generate open paranthesis as along as open < n
-        // generate close paranthesis as long as close < open
-        
         vector<string> res;
         recurse(0, 0, n, "", res);
         return res;
     }
 private:
     void recurse(int open, int close, int n, string par, vector<string>& res) {
-        if (open == close && close == n) {
+        if (open == n && open == close) {
             res.push_back(par);
             return;
         }
         
-        if (open < n) {
-            recurse(open + 1, close, n, par + '(', res);
-        }
-        
-        if (close < open) {
-            recurse(open, close + 1, n, par + ')', res);
-        }
+        if (open < n) recurse(open + 1, close, n, par + '(', res);
+        if (close < open) recurse(open, close + 1, n, par + ')', res);
     }
 };
