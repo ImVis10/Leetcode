0224af1e49f0d38e2e5cddae9293015b3728e3ec
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-12-22 20:37:55 -0500

    Time: 0 ms (100.00%), Space: 7.3 MB (35.91%) - LeetHub

---
+++ 1496-path-crossing/1496-path-crossing.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isPathCrossing(string path) {
+        pair<int, int> curr = {0, 0};
+        
+        set<pair<int, int>> set;
+        set.insert(curr);
+        
+        for (char dir : path) {
+            if (dir == 'N') {
+                curr.second += 1;
+            } else if (dir == 'E') {
+                curr.first += 1;
+            } else if (dir == 'W') {
+                curr.first -= 1;
+            } else {
+                curr.second -= 1;
+            }
+            
+            if (set.find(curr) != set.end()) return true;
+            
+            set.insert(curr);
+        }
+        return false;
+    }
+};
