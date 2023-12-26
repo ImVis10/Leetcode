cf6f1728f000729055e9f9fe401be1b7ffa8e9e8
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-12-25 23:15:21 -0500

    Time: 360 ms (6.94%), Space: 129.5 MB (47.14%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.cpp
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.cpp
@@ -1,34 +1,30 @@
 class TimeMap {
 public:
-    unordered_map<string, vector<pair<string, int>>> map;
     TimeMap() {
         
     }
     
     void set(string key, string value, int timestamp) {
-        map[key].push_back({value, timestamp});
+        mp[key].push_back({value, timestamp});
     }
     
     string get(string key, int timestamp) {
-        if (map.count(key) == 0) { // if key is not present in the map
-            return "";
-        }
-        
-        int low = 0, high = map[key].size() - 1;
-        string res = "";
-        
+        if (mp.find(key) == mp.end()) return "";
+
+        int low = 0, high = mp[key].size() - 1;
+        string val = "";
         while (low <= high) {
             int mid = low + (high - low) / 2;
-            
-            if (timestamp < map[key][mid].second) {
-                high = mid - 1;
-            } else if (map[key][mid].second <= timestamp) {
+            if (mp[key][mid].second > timestamp) high = mid - 1;
+            else {
                 low = mid + 1;
-                res = map[key][mid].first;
+                val = mp[key][mid].first;
             }
         }
-        return res;
+        return val;
     }
+private:
+    unordered_map<string, vector<pair<string, int>>> mp;
 };
 
 /**
