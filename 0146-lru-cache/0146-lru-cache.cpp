cdbf4c4ec1ed05e30efd537855295a53d4b0f5ec
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-12-26 00:17:58 -0500

    Time: 356 ms (80.22%), Space: 171.1 MB (57.92%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -2,75 +2,68 @@ class Node {
 public:
     int key;
     int val;
-    Node* next;
     Node* prev;
-    
+    Node* next;
+
     Node(int key, int val) {
         this->key = key;
         this->val = val;
+        this->prev = NULL;
         this->next = NULL;
-        this-> prev = NULL;
     }
 };
-
 class LRUCache {
 public:
     LRUCache(int capacity) {
         cap = capacity;
-        start = new Node(0, 0); // dummy node to indicate start
-        end = new Node(0, 0); // dummy node to indicate end
-        
-        start->next = end;
-        end->prev = start;
+        head = new Node(0, 0);
+        tail = new Node(0, 0);
+
+        head->next = tail;
+        tail->prev = head;
     }
     
     int get(int key) {
-        if (cache.find(key) == cache.end()) return -1;
-        // move the Node to the most recently used part of the list i.e. to the right, so remove first and then insert
-        remove(cache[key]);
-        insert(cache[key]);
-        return cache[key]->val;
+        if (mp.find(key) == mp.end()) return -1;
+        remove(mp[key]);
+        insert(mp[key]);
+        return mp[key]->val;
     }
     
     void put(int key, int value) {
-        if (cache.find(key) != cache.end()) {
-            Node* node = cache[key];
-            remove(node);
-            delete node; // free the allocated memory
-        }
-        
-        cache[key] = new Node(key, value);
-        insert(cache[key]);
-        
-        if (cache.size() > cap) {
-            Node* lru = start->next;
+        if (mp.find(key) != mp.end()) {
+            remove(mp[key]);
+            delete mp[key];
+        } else if (mp.size() == cap) {
+            Node* lru = head->next;
             remove(lru);
-            cache.erase(lru->key); // erase the entry from the map
-            delete lru; // free the allocated memory
+            mp.erase(lru->key);
+            delete lru;
         }
+        mp[key] = new Node(key, value);
+        insert(mp[key]);
     }
 private:
-    unordered_map<int, Node*> cache;
-    Node* start;
-    Node* end;
     int cap;
-    
+    Node* head;
+    Node* tail;
+    unordered_map<int, Node*> mp;
+
     void remove(Node* node) {
-        Node* next = node->next;
-        Node* prev = node->prev;
-        
-        next->prev = prev;
-        prev->next = next;
+        Node* prevNode = node->prev;
+        Node* nextNode = node->next;
+
+        prevNode->next = nextNode;
+        nextNode->prev = prevNode;
     }
-    
+
     void insert(Node* node) {
-        Node* prev = end->prev;
-        
-        node->next = end;
-        node->prev = prev;
-        
-        prev->next = node;
-        end->prev = node;
+        Node* beforeTail = tail->prev;
+        beforeTail->next = node;
+        node->prev = beforeTail;
+
+        node->next = tail;
+        tail->prev = node;
     }
 };
 
