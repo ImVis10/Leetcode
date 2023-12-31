2778401653c04108023d093955635a2f70bc818e
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2023-12-30 19:28:58 -0500

    Time: 0 ms (100.00%), Space: 6.9 MB (11.97%) - LeetHub

--- 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.cpp
+++ 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     int maxLengthBetweenEqualCharacters(string s) {
-        unordered_map<char, vector<int>> mp;
+        unordered_map<char, int> mp;
         int res = -1;
         
         for (int i = 0; i < s.length(); i++) {
-            mp[s[i]].push_back(i);
-        }
-        
-        for (auto &[ch, pos] : mp) {
-            if (pos.size() > 1) {
-                res = max(res, pos[pos.size() - 1] - pos[0] - 1);
+            if (mp.find(s[i]) != mp.end()) {
+                res = max(res, i - mp[s[i]] - 1);
+            } else {
+                mp[s[i]] = i;
             }
         }
         return res;
