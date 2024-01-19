1d7af9c6b8dff48ba2d86297f67a2c62eb5591ac
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2024-01-18 22:35:05 -0500

    Time: 11 ms (80.66%), Space: 11 MB (61.52%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -2,23 +2,23 @@ class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        vector<vector<int>> cache(n, vector<int>(n, -101));
+        vector<vector<int>> cache(n, vector<int> (n, -101));
         int minPath = INT_MAX;
         for (int col = 0; col < n; col++) {
-            minPath = min(minPath, recurse(n - 1, col, matrix, cache));
+            minPath = min(minPath, recurse(0, col, matrix, cache));
         }
         return minPath;
     }
 private:
-    int recurse(int row, int col, vector<vector<int>>& grid, vector<vector<int>>& cache) {
-        if (col < 0 || col > grid.size() - 1) return 1e9;
-        if (row == 0) return grid[row][col];
+    int recurse(int row, int col, vector<vector<int>>& matrix, vector<vector<int>>& cache) {
+        if (col < 0 or col >= matrix.size()) return 1e9;
+        if (row == matrix.size() - 1) return matrix[row][col];
         if (cache[row][col] != -101) return cache[row][col];
         
-        int leftDiag = grid[row][col] + recurse(row -1, col - 1, grid, cache);
-        int up = grid[row][col] + recurse(row - 1, col, grid, cache);
-        int rightDiag = grid[row][col] + recurse(row -1, col + 1, grid, cache);
+        int leftDiagnol = matrix[row][col] + recurse(row + 1, col - 1, matrix, cache);
+        int down = matrix[row][col] + recurse(row + 1, col, matrix, cache);
+        int rightDiagnol = matrix[row][col] + recurse(row + 1, col + 1, matrix, cache);
         
-        return cache[row][col] = min({leftDiag, up, rightDiag});
+        return cache[row][col] = min(leftDiagnol, min(down, rightDiagnol));
     }
 };
