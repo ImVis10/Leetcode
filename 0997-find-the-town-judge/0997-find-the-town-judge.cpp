613eefcf9f7aa382acec584d1995df7272904edf
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2024-02-21 21:12:47 -0500

    Time: 109 ms (95.82%), Space: 64.9 MB (57.87%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -1,25 +1,16 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        if (trust.size() < n - 1) { // every other person apart from the judge trusts judge, so size of the trust array should be minimum n - 1
-            return -1;
+        int normalPeople = trust.size();
+        if (normalPeople < n - 1 or normalPeople == n) return -1;
+        if (normalPeople == 0 and n == 1) return 1;
+        unordered_map<int, int> numTrust;
+        for (auto& persons : trust) {
+            numTrust[persons[0]]--;
+            numTrust[persons[1]]++;
         }
-        
-        if (trust.size() == 0 && n == 1) {
-            return n;
-        }
-        
-        map<int, int> inCount; 
-        
-        for (auto pair : trust) {
-            inCount[pair[0]]--;
-            inCount[pair[1]]++;
-        }
-        
-        for (auto entry : inCount) {
-            if (entry.second == n - 1) {
-                return entry.first;
-            }
+        for (auto &[k, v] : numTrust) {
+            if (v == n - 1) return k;
         }
         return -1;
     }
