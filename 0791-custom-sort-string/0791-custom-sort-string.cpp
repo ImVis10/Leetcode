7ebc44c1d981febf66a93bc7acf6032b3a8e1c15
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2024-03-11 16:54:05 -0500

    Time: 0 ms (100.00%), Space: 8.5 MB (23.93%) - LeetHub

---
+++ 0791-custom-sort-string/0791-custom-sort-string.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string customSortString(string order, string s) {
+        string res = "";
+        unordered_map<char, int> mp;
+        for (char ch : s) mp[ch]++;
+        for (char ch : order) {
+            if (mp.find(ch) != mp.end() and mp[ch] != 0) {
+                while (mp[ch] > 0) {
+                    res += ch;
+                    mp[ch] -= 1;
+                }
+            }
+        }
+        for (auto [ch, cnt] : mp) {
+            while (cnt > 0) {
+                res += ch;
+                cnt -= 1;
+            }
+        }
+        return res;
+    }
+};
