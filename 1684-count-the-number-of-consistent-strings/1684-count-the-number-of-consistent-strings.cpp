ef07a6222f4d640032aa6469653cc32f36dbc221
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2024-09-11 21:42:40 -0400

    Time: 48 ms (51.57%), Space: 33.8 MB (87.14%) - LeetHub

--- 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.cpp
+++ 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.cpp
@@ -1,15 +1,35 @@
 class Solution {
 public:
     int countConsistentStrings(string allowed, vector<string>& words) {
-        // instead of counting consistent strings, we will count inconsistent strings.
-        // all strings - #inconsistent strings is our result
-        int n = words.size();
-        int res = n;
-        vector<int> chars(26);
-        for (char& ch : allowed) chars[ch - 'a'] = 1;
+        // // instead of counting consistent strings, we will count inconsistent strings.
+        // // all strings - #inconsistent strings is our result
+        // int n = words.size();
+        // int res = n;
+        // vector<int> chars(26);
+        // for (char& ch : allowed) chars[ch - 'a'] = 1;
+        // for (string& word : words) {
+        //     for (char& ch : word) {
+        //         if (chars[ch - 'a'] != 1) {
+        //             res--;
+        //             break;
+        //         }
+        //     }
+        // }
+        // return res;
+        
+        // leetcode discuss section solution with just O(1) SPACE
+        // INSTEAD OF VECTOR AS A MAP, USING AN INTEGER AS A MAP
+        // INTEGER HAS 32 BITS, WE HAVE 26 CHARS SO WE HAVE ENOUGH BITS FOR EACH CHAR
+        // 1 bit set against the chars in allowed
+        int mp = 0;
+        int res = words.size();
+        for (char& ch: allowed) {
+            mp |= (1 << (ch -'a'));
+        }
+        
         for (string& word : words) {
             for (char& ch : word) {
-                if (chars[ch - 'a'] != 1) {
+                if ((mp & (1 << (ch - 'a'))) == 0) {
                     res--;
                     break;
                 }
