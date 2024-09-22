da5ebc3d3d48ddb4791fcc2bbdba2c033f4a46f4
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2024-09-21 23:17:30 -0500

    Time: 2 ms (53.78%), Space: 7.5 MB (54.58%) - LeetHub

--- 0440-k-th-smallest-in-lexicographical-order/0440-k-th-smallest-in-lexicographical-order.cpp
+++ 0440-k-th-smallest-in-lexicographical-order/0440-k-th-smallest-in-lexicographical-order.cpp
@@ -19,7 +19,7 @@ private:
     int numNodes(long long curr, int n) {
         long long nei = curr + 1, cnt = 0;
         while (curr <= n) {
-            cnt += min(nei, (long long) (n +  1)) - curr;
+            cnt += min(nei, (long long) (n +  1)) - curr; // number of nodes in a level
             curr *= 10;
             nei *= 10;
         }
