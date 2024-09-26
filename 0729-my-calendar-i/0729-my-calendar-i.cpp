da9b7fc0de92072d210bae5db94659c4db446128
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2024-09-26 08:07:50 -0500

    Time: 67 ms (90.56%), Space: 42.1 MB (72.41%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.cpp
+++ 0729-my-calendar-i/0729-my-calendar-i.cpp
@@ -1,18 +1,47 @@
-class MyCalendar {
-    vector<pair<int, int>> bookings;
+class Tree {
 public:
-    MyCalendar() {
-
-    }
+    Tree* left;
+    Tree* right;
+    int start, end;
     
-    bool book(int start, int end) {
-        for (auto& [s, e] : bookings) {
-            if (!(start >= e or end <= s)) { // checks for overlap between new event and existing event
+    Tree(int start, int end) : start(start), end(end), left(nullptr), right(nullptr) {}
+    
+    bool insert(int start, int end) {
+        Tree* curr = this;
+        while (true) {
+            if (end <= curr->start) { // left subtree
+                if (!curr->left) {
+                    curr->left = new Tree(start, end);
+                    return true;
+                }
+                curr = curr->left;
+            } else if (start >= curr->end) { // right subtree
+                if (!curr->right) {
+                    curr->right = new Tree(start, end);
+                    return true;
+                }
+                curr = curr->right;
+            } else {
                 return false;
             }
         }
-        bookings.push_back({start, end});
-        return true;
+        return false;
+    }
+};
+
+class MyCalendar {
+public:
+    Tree* root;
+    MyCalendar() {
+        root = nullptr;
+    }
+    
+    bool book(int start, int end) {
+        if (!root) {
+            root = new Tree(start, end);
+            return true;
+        }
+        return root->insert(start, end);
     }
 };
 
