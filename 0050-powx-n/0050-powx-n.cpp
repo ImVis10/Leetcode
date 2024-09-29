13310c699c34016c96f4f03d255a8cb543cb0706
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2024-09-29 09:08:38 -0500

    Time: 0 ms (100.00%), Space: 8.4 MB (42.97%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        if (n == 0 || x == 1) return 1;
-        else if (n == 1) return x;
+        if (n == 0 or x == 1) return 1;
+        if (x == 1) return 1;
         
         long long longN = n;
         
         if (n < 0) {
             x = 1 / x;
-            longN = -longN;
+            longN = -longN; 
         }
         
-        double res = myPow(x, longN / 2);
-        res *= res;
-        
-        if (longN % 2 == 1) res *= x;
+        double res = 1;
+        while (longN) {
+            if (longN % 2 == 1) {
+                res *= x;
+                longN--;
+            }
+            x *= x;
+            longN /= 2;
+        }
         return res;
     }
 };
