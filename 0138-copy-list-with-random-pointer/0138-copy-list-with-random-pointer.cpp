eddc39d4676c918998045c97f537226fdf14c818
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2024-09-30 22:11:46 -0500

    Time: 6 ms (56.91%), Space: 14.5 MB (93.80%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -17,15 +17,44 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        if (!head) return NULL;
-        if (ll.find(head) == ll.end()) { // if node not cloned yet
-            Node* node = new Node(head->val, NULL, NULL);
-            ll[head] = node;
-            node->next = this->copyRandomList(head->next); // clone the next node
-            node->random = this->copyRandomList(head->random); // clone the random node
+        // if (!head) return NULL;
+        // if (ll.find(head) == ll.end()) { // if node not cloned yet
+        //     Node* node = new Node(head->val, NULL, NULL);
+        //     ll[head] = node;
+        //     node->next = this->copyRandomList(head->next); // clone the next node
+        //     node->random = this->copyRandomList(head->random); // clone the random node
+        // }
+        // return ll[head];
+        
+        // WITHOUT USING EXTRA SPACE
+        // INSTEAD OF A MAP, WE CAN TWEAK THE ORIGINAL LINKED LIST TO HAVE THE CLONED NODE NEXT TO THE ORIGINAL NODE
+        
+        if (!head) return nullptr;
+        Node* ptr = head;
+        while (ptr) {
+            Node* newNode = new Node(ptr->val, nullptr, nullptr);
+            newNode->next = ptr->next;
+            ptr->next = newNode;
+            ptr = newNode->next;
         }
-        return ll[head];
+        // link random pointers
+        ptr = head;
+        while (ptr) {
+            ptr->next->random = ptr->random ? ptr->random->next : nullptr;
+            ptr = ptr->next->next;
+        }
+        // link next pointers
+        Node* p1 = head;
+        Node* p2 = head->next;
+        Node* res = head->next;
+        while (p1) {
+            p1->next = p1->next->next;
+            p2->next = p2->next ? p2->next->next : nullptr;
+            p1 = p1->next;
+            p2 = p2->next;
+        }
+        return res;
     }
-private:
-    unordered_map<Node*, Node*> ll;
+// private:
+//     unordered_map<Node*, Node*> ll;
 };
