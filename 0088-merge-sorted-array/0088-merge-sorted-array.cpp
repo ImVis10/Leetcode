ea8e8221c75a59d45d0e9bedc80f4350fefff12c
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2024-10-01 07:15:41 -0500

    Time: 6 ms (7.19%), Space: 11.3 MB (68.83%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,35 +1,27 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        // Interview Tip: Whenever you're trying to solve an array problem in place, always consider the possibility of iterating backwards instead of forwards through the array. It can completely change the problem, and make it a lot easier.
+        // use the zeroes at the end to store data i.e. overwrite them
+        // so start iterating nums1 from m - 1 and nums2 from n - 1
+        
         if (m == 0) {
             nums1 = nums2;
             return;
         }
-
-        vector<int> temp(nums1.begin(), nums1.end());
         
-        int i = 0, j = 0, idx = 0;
-        
-        while (i < m and j < n) {
-            if (temp[i] == nums2[j]) {
-                nums1[idx++] = temp[i];
-                nums1[idx++] = temp[i];
-                i++;
-                j++;
-            } else if (temp[i] < nums2[j]) {
-                nums1[idx++] = temp[i];
-                i++;
-            } else {
-                nums1[idx++] = nums2[j];
-                j++;
+        int i = m - 1, j = n - 1;
+        for (int p = m + n - 1; p >= 0; p--) {
+            if (j < 0) {
+                break;
             }
-        }
-
-        while (idx < m + n and (i < m or j < n)) {
-            if (i < m)
-                nums1[idx++] = temp[i++];
-            else 
-                nums1[idx++] = nums2[j++];
+            if (i >= 0 and nums1[i] > nums2[j]) {
+                nums1[p] = nums1[i];
+                i--;
+            } else {
+                nums1[p] = nums2[j];
+                j--;
+            }
         }
     }
 };
