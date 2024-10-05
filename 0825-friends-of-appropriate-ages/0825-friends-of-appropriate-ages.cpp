e31f200e9cd5f5d473aab9de06a938c27feddae7
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2024-10-05 17:35:42 -0500

    Time: 38 ms (57.94%), Space: 41.1 MB (51.72%) - LeetHub

--- 0825-friends-of-appropriate-ages/0825-friends-of-appropriate-ages.cpp
+++ 0825-friends-of-appropriate-ages/0825-friends-of-appropriate-ages.cpp
@@ -2,13 +2,19 @@ class Solution {
 public:
     int numFriendRequests(vector<int>& ages) {
         int res = 0;
-        sort(ages.begin(), ages.end());
-        for (int& age : ages) {
-            auto idx1 = upper_bound(ages.begin(), ages.end(), age) - ages.begin();
-            auto idx2 = upper_bound(ages.begin(), ages.end(), (0.5 * age) + 7) - ages.begin();
-            if (idx1 > idx2) res += idx1 - idx2 - 1;
+        unordered_map<int, int> freq;
+        for (int& age : ages) freq[age]++;
+        for (auto& [x, cnt1] : freq) {
+            for (auto& [y, cnt2] : freq) {
+                if (request(x, y)) {
+                    res += cnt1 * (cnt2 - (x == y ? 1 : 0));
+                }
+            }
         }
         return res;
-        
+    }
+private:
+    bool request(int x, int y) {
+        return !(y <= (0.5 * x) + 7 or y > x or (y > 100 and x  < 100));
     }
 };
