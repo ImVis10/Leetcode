9e34baf5d8c67b29ac1df0a181db8a2b189652ca
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2024-10-10 21:04:23 -0500

    Time: 134 ms (83.16%), Space: 58.2 MB (86.60%) - LeetHub

--- 1942-the-number-of-the-smallest-unoccupied-chair/1942-the-number-of-the-smallest-unoccupied-chair.cpp
+++ 1942-the-number-of-the-smallest-unoccupied-chair/1942-the-number-of-the-smallest-unoccupied-chair.cpp
@@ -7,17 +7,17 @@ public:
         
         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
         int chair = 0;
-        set<int> chairs;
+        set<int> releasedChairs;
         for (auto& time : times) {
             int start = time[0], end = time[1];
             while (!pq.empty() and pq.top().first <= start) { // release chairs based on arrival time of current friend
-                chairs.insert(pq.top().second);
+                releasedChairs.insert(pq.top().second);
                 pq.pop();
             }
             int currChair;
-            if (!chairs.empty()) { // if chairs are free, get the first available chair
-                currChair = *chairs.begin();
-                chairs.erase(chairs.begin());
+            if (!releasedChairs.empty()) { // if chairs are free, get the first available chair
+                currChair = *releasedChairs.begin();
+                releasedChairs.erase(releasedChairs.begin());
             } else { // if no chair is free, get a new chair
                 currChair = chair++;
             }
