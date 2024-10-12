4cb52c79d42ddfcdca02030ca8b809b0c95affd9
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2024-10-11 21:29:57 -0500

    Time: 288 ms (58.11%), Space: 92.7 MB (93.47%) - LeetHub

--- 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.cpp
+++ 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     int minGroups(vector<vector<int>>& intervals) {
+        // same as meeting rooms ii
         sort(intervals.begin(), intervals.end());
+        int groups = 0;
         
-        priority_queue<int, vector<int>, greater<int>> pq;
-        
-        int rooms = 0;
+        priority_queue<int, vector<int>, greater<int>> pq; // min heap to store end times
         
         for (int i = 0; i < intervals.size(); i++) {
-            if (pq.empty() or pq.top() >= intervals[i][0]) { // allocate new room
-                rooms++;
-            } else {
-                pq.pop();
-            }
+            if (pq.empty() or pq.top() >= intervals[i][0]) groups++;
+            else pq.pop();
             pq.push(intervals[i][1]);
         }
-        return rooms;
+        return groups;
     }
 };
