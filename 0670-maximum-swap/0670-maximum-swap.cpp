1181bf2dd034b233ab1b2665d78a1b94ea9b1932
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2024-10-17 06:23:44 -0500

    Time: 3 ms (38.18%), Space: 7.6 MB (31.77%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.cpp
+++ 0670-maximum-swap/0670-maximum-swap.cpp
@@ -2,22 +2,27 @@ class Solution {
 public:
     int maximumSwap(int num) {
         string s = to_string(num);
-        int n = s.length();
         int maxIdx = -1, idx1 = -1, idx2 = -1;
+        
         char maxCh = '/';
-        for (int i = n - 1; i >= 0; i--) {
+        
+        for (int i = s.length() - 1; i >=0; i--) {
             char ch = s[i];
             if (ch > maxCh) {
                 maxCh = ch;
                 maxIdx = i;
                 continue;
             }
+            
             if (ch < maxCh) {
                 idx1 = i;
                 idx2 = maxIdx;
+                cout << idx1 << endl;
+                cout << idx2 << endl;
             }
         }
-        if (idx1 == -1) return stoi(s);
+        
+        if (idx1 == -1 or idx2 == -1) return stoi(s);
         swap(s[idx1], s[idx2]);
         return stoi(s);
     }
