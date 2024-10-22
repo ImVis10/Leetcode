131bdfedb356ef5a86fcb12c807598ce534e5157
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2024-10-21 19:38:50 -0500

    Time: 14 ms (99.42%), Space: 12.5 MB (97.98%) - LeetHub

--- 1593-split-a-string-into-the-max-number-of-unique-substrings/1593-split-a-string-into-the-max-number-of-unique-substrings.cpp
+++ 1593-split-a-string-into-the-max-number-of-unique-substrings/1593-split-a-string-into-the-max-number-of-unique-substrings.cpp
@@ -2,21 +2,24 @@ class Solution {
 public:
     int maxUniqueSplit(string s) {
         unordered_set<string> set;
-        return backtrack(s, 0, set);
+        int maxSplits = 0;
+        backtrack(s, 0, set, 0, maxSplits);
+        return maxSplits;
     }
 private:
-    int backtrack(string& s, int idx, unordered_set<string>& set) {
-        if (idx == s.size()) return 0;
-        int maxSplits = 0;
-        for (int i = idx; i < s.size(); i++) {
-            string substr = s.substr(idx, i - idx + 1);
+    void backtrack(string& s, int start, unordered_set<string>& set, int splits, int& maxSplits) {
+        if (splits + (s.size() - start) <= maxSplits) return;
+        if (start == s.size()) {
+            maxSplits = max(maxSplits, splits);
+            return;
+        }
+        for (int end = start + 1; end <= s.size(); end++) {
+            string substr = s.substr(start, end - start);
             if (set.find(substr) == set.end()) {
                 set.insert(substr);
-                int splits = 1 + backtrack(s, i + 1, set);
+                backtrack(s, end, set, splits + 1, maxSplits);
                 set.erase(substr);
-                maxSplits = max(splits, maxSplits);
             }
         }
-        return maxSplits;
     }
 };
