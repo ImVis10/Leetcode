c9d70b6a9713d461cd096f0c9790d294a7e859d0
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2024-11-03 08:14:51 -0600

    Time: 0 ms (100.00%), Space: 7.5 MB (83.69%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -1,40 +1,11 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        if (s == goal) {
-            return true;
-        }
+        int m = s.length(), n = goal.length();
+        if (m != n) return false;
         
-        string temp = s;
+        string temp = s + s;
         
-        s = removespace(s);
-        goal = removespace(goal);
-        
-        int len = s.length();
-
-        for (int i = 0; i < len; i++) {
-            if (s != goal) {
-                s = temp;
-                reverse(s.begin(), s.end());
-                reverse(s.begin(), s.begin() + len - i - 1);
-                reverse(s.begin() + len - i - 1, s.end());
-            } else {
-                return true;
-            }
-        }
-        return false;
-    }
-
-private:
-    string removespace(string s) {
-        //traversing the string
-        for (int i = 0; i < s.length(); i++) {
-            if (!(s[i] >= 'a' && s[i] <= 'z')) {
-                //using in-built function to erase element
-                s.erase(s.begin() + i);
-                i--;
-            }
-        }
-        return s;
+        return temp.find(goal) < 2 * m;
     }
 };
