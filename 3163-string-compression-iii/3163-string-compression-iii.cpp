dd842761ac3a7c2e9aa49798fc73594b3194da41
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2024-11-03 18:30:48 -0600

    Time: 60 ms (21.37%), Space: 29.8 MB (57.63%) - LeetHub

---
+++ 3163-string-compression-iii/3163-string-compression-iii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string compressedString(string word) {
+        /**
+        aaaaaaaaaaaaaabb
+        123456789abcde    
+        9a5a1b1c1d1e
+        **/
+        string comp;
+        int n = word.length();
+        for (int i = 0, j = 0; j < n; j++) {
+            if ((word[j] == word[j + 1] and j - i + 1 == 9) or word[j] != word[j + 1]) {
+                comp += to_string(j - i + 1);
+                comp += word[j];
+                i = j + 1;
+            } else {
+                continue;
+            }
+        }
+        return comp;
+    }
+};
