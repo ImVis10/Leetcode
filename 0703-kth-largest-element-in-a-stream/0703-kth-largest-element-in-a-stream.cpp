eec3ec59de5e40e8a4afb43b805d3663502ed388
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2024-11-04 19:19:02 -0600

    Time: 1144 ms (5.07%), Space: 31.6 MB (5.84%) - LeetHub

--- 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.cpp
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.cpp
@@ -2,17 +2,40 @@ class KthLargest {
 public:
     KthLargest(int k, vector<int>& nums) {
         this->k = k;
-        for (auto& num : nums) pq.push(num);
+        arr = nums;
     }
     
     int add(int val) {
-        pq.push(val);
-        while (pq.size() > k) pq.pop();
-        return pq.top();
+        arr.push_back(val);
+        return quickselect(arr, 0, arr.size() - 1, arr.size() - k);
     }
 private:
     int k;
-    priority_queue<int, vector<int>, greater<int>> pq;
+    vector<int> arr;
+    
+    int partition(vector<int>& arr, int left, int right) {
+        int pivotIdx = left + rand() % (right - left + 1);
+        int pivotVal = arr[pivotIdx];
+        swap(arr[pivotIdx], arr[right]);
+        
+        int storeIdx = left;
+        for (int i = left; i < right; i++) {
+            if (arr[i] < pivotVal) {
+                swap(arr[i], arr[storeIdx]);
+                storeIdx++;
+            }
+        }
+        swap(arr[storeIdx], arr[right]);
+        return storeIdx;
+    }
+    
+    int quickselect(vector<int>& arr, int left, int right, int k) {
+        if (left == right) return arr[left];
+        int pivotIdx = partition(arr, left, right);
+        if (k == pivotIdx) return arr[k];
+        else if (k < pivotIdx) return quickselect(arr, left, pivotIdx - 1, k);
+        else return quickselect(arr, pivotIdx + 1, right, k);
+    }
 };
 
 /**
