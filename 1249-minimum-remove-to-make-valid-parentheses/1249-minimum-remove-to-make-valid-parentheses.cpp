cdadd7452a5b118501196cb97355250e439b482f
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2024-11-08 19:57:59 -0600

    Time: 7 ms (72.26%), Space: 12.4 MB (85.87%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -2,30 +2,24 @@ class Solution {
 public:
     string minRemoveToMakeValid(string s) {
         int n = s.length();
-        int open = 0;
-        // to find extra closed braces
-        for (int i = 0; i < n; i++) {
-            if (s[i] == '(') open++;
-            else if (s[i] == ')') {
-                if (open == 0) s[i] = '$'; 
-                else open--;
-            }
+        int open = 0, close = 0;
+        string res;
+        
+        for (char& ch : s) {
+            if (ch == ')') close++;
         }
         
-        // to find extra open braces
-        int close = 0;
-        for (int i = n - 1; i >= 0; i--) {
-            if (s[i] == ')') close++; 
-            else if (s[i] == '(') {
-                if (close == 0) s[i] = '$';
-                else close--;
+        for (char& ch : s) {
+            if (ch == '(') {
+                if (open == close) continue;
+                open++;
+            } else if (ch == ')') {
+                close--;
+                if (open == 0) continue;
+                open--;
             }
-        }
-        string res;
-        for (char ch : s) {
-            if (ch != '$') {
-                res += ch;
-            }
+            
+            res += ch;
         }
         return res;
     }
