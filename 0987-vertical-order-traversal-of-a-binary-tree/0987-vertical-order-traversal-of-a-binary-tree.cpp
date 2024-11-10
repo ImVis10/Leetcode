87a0492848a223742c3505723dcec8abdcd97eae
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2024-11-09 19:42:40 -0600

    Time: 0 ms (100.00%), Space: 14.3 MB (75.26%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -12,19 +12,26 @@
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
+        // same as leetcode 314, but the difference is for any two nodes in same row and col, they must be sorted in ascending order
+        // so we need two things different from leetcode 314 where we only consider column
+        // here we should consider both row and column
+        // and also for the map, the key should be column (same as in 314) but the value must be a map of <row, set> as we need values in sorted order
+        // that too a multiset as there might be duplicate numbers for nodes
+        
         if (!root) return {};
         unordered_map<int, map<int, multiset<int>>> mp;
-        int minCol = 0, maxCol = 0;
-        queue<pair<pair<int,int>, TreeNode*>> q;
+        queue<pair<pair<int, int>, TreeNode*>> q;
+        int minCol = INT_MAX, maxCol = INT_MIN;
+        
         q.push({{0, 0}, root});
         
         while (!q.empty()) {
-            auto curr = q.front(); q.pop();
-            int row = curr.first.first, col = curr.first.second;
-            minCol = min(col, minCol);
-            maxCol = max(col, maxCol);
-            TreeNode* node = curr.second;
+            auto p = q.front(); q.pop();
+            int row = p.first.first, col = p.first.second;
+            TreeNode* node = p.second;
             mp[col][row].insert(node->val);
+            minCol = min(minCol, col);
+            maxCol = max(maxCol, col);
             if (node->left) q.push({{row + 1, col - 1}, node->left});
             if (node->right) q.push({{row + 1, col + 1}, node->right});
         }
@@ -32,8 +39,8 @@ public:
         vector<vector<int>> res;
         for (int col = minCol; col <= maxCol; col++) {
             vector<int> colNodes;
-            for (auto& row : mp[col]) {
-                colNodes.insert(colNodes.end(), row.second.begin(), row.second.end());
+            for (auto& [row, nodes] : mp[col]) {
+                colNodes.insert(colNodes.end(), nodes.begin(), nodes.end());
             }
             res.push_back(colNodes);
         }
