c20b83d671b33ec3743dea0254a2827ff767c5aa
Venkata Viswanath Vankadara <41447494+ImVis10@users.noreply.github.com>
2024-11-20 17:59:16 -0600

    Time: 0 ms (100.00%), Space: 22.6 MB (29.46%) - LeetHub

--- 0986-interval-list-intersections/0986-interval-list-intersections.cpp
+++ 0986-interval-list-intersections/0986-interval-list-intersections.cpp
@@ -5,9 +5,8 @@ public:
         vector<vector<int>> res;
         
         while (i < m and j < n) {
-                if ((a[i][0] <= b[j][0] and b[j][0] <= a[i][1]) or (b[j][0] <= a[i][0] and a[i][0] <= b[j][1])) {
-                               res.push_back({max(a[i][0], b[j][0]), min(a[i][1], b[j][1])});
-                }
+                int start = max(a[i][0], b[j][0]), end = min(a[i][1], b[j][1]);
+                if (start <= end) res.push_back({start, end});
                 if (a[i][1] < b[j][1]) i++;
                 else j++; 
         }
